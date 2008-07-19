88ab225da519f1ed2b0de41c3272edab1a4155b6
elaskavaia <elaskavaia>
2008-07-19 22:00:20 +0000

    added add to operation, fixed bugs

--- com.reflexit.magiccards.core/src/com/reflexit/magiccards/core/model/nav/Deck.java
+++ com.reflexit.magiccards.core/src/com/reflexit/magiccards/core/model/nav/Deck.java
@@ -1,12 +1,31 @@
 package com.reflexit.magiccards.core.model.nav;
 
 import com.reflexit.magiccards.core.model.ICardDeck;
-import com.reflexit.magiccards.core.model.IMagicCard;
 
 public class Deck extends CardElement {
-	private ICardDeck<IMagicCard> store;
+	private ICardDeck store;
 
 	public Deck(String filename, CardOrganizer parent) {
 		super(filename, parent);
+	}
+
+	public ICardDeck getStore() {
+		return this.store;
+	}
+
+	public void open(ICardDeck store) {
+		if (this.store == null) {
+			this.store = store;
+		} else {
+			throw new IllegalArgumentException("Already open");
+		}
+	}
+
+	public void close() {
+		this.store = null;
+	}
+
+	public boolean isOpen() {
+		return this.store != null;
 	}
 }
