ebc659dad2d06a0ee69eab8a53c0921f77a6c41c
elaskavaia <elaskavaia>
2008-07-31 05:59:40 +0000

    - Added card count

---
+++ com.reflexit.magiccards.core/src/com/reflexit/magiccards/core/model/ICardCountable.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Alena Laskavaia.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Alena Laskavaia - initial API and implementation
+ *******************************************************************************/
+package com.reflexit.magiccards.core.model;
+
+/**
+ * @author Alena
+ * Interface for cards and card collection to return total card count
+ */
+public interface ICardCountable {
+	int getCount();
+}
