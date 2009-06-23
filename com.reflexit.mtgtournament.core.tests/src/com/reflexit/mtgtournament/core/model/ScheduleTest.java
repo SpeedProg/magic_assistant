6f9328f013e5a44711b38fb59c7ab8d46b60f73e
elaskavaia <elaskavaia>
2009-06-23 03:46:35 +0000

    - fixed swiss and add several more schedulers

--- com.reflexit.mtgtournament.core.tests/src/com/reflexit/mtgtournament/core/model/ScheduleTest.java
+++ com.reflexit.mtgtournament.core.tests/src/com/reflexit/mtgtournament/core/model/ScheduleTest.java
@@ -51,19 +51,9 @@ public class ScheduleTest extends TestCase {
 
 	public void testSwiss() {
 		Tournament tour = new Tournament();
-		tour.setType(TournamentType.SWISS, 3, true);
+		tour.setType(TournamentType.SWISS, 5, true);
 		tour.generatePlayers(6);
-		tour.schedule();
-		for (int i = 0; i <= tour.getNumberOfRounds(); i++) {
-			Round r = tour.getRound(i);
-			r.schedule();
-			if (i > 0) {
-				generateWinnigs(r);
-			}
-			r.close();
-		}
-		tour.printSchedule(System.out);
-		checkPlayedBefore(tour, -1);
+		scheduleAll(tour);
 	}
 
 	/**
@@ -89,5 +79,71 @@ public class ScheduleTest extends TestCase {
 				ti.getP2().setWinGames(1);
 			}
 		}
+	}
+
+	public void testSwiss_odd() {
+		Tournament tour = new Tournament();
+		tour.setType(TournamentType.SWISS, 6, true);
+		tour.generatePlayers(7);
+		scheduleAll(tour);
+	}
+
+	public void testSwiss_More() {
+		Tournament tour = new Tournament();
+		tour.setType(TournamentType.SWISS, 6, true);
+		tour.generatePlayers(30);
+		scheduleAll(tour);
+	}
+
+	public void testElimination_8() {
+		Tournament tour = new Tournament();
+		tour.setType(TournamentType.ELIMINATION, 3, true);
+		tour.generatePlayers(8);
+		scheduleAll(tour);
+	}
+
+	private void scheduleAll(Tournament tour) {
+		tour.schedule();
+		for (int i = 0; i <= tour.getNumberOfRounds(); i++) {
+			Round r = tour.getRound(i);
+			r.schedule();
+			if (i > 0) {
+				generateWinnigs(r);
+			}
+			r.close();
+		}
+		tour.printSchedule(System.out);
+		checkPlayedBefore(tour, -1);
+	}
+
+	public void testElimination_Opt8() {
+		Tournament tour = new Tournament();
+		tour.setType(TournamentType.ELIMINATION, 0, true);
+		tour.generatePlayers(8);
+		tour.schedule();
+		assertEquals(3, tour.getNumberOfRounds());
+	}
+
+	public void testElimination_Opt7() {
+		Tournament tour = new Tournament();
+		tour.setType(TournamentType.ELIMINATION, 0, true);
+		tour.generatePlayers(7);
+		tour.schedule();
+		assertEquals(3, tour.getNumberOfRounds());
+	}
+
+	public void testElimination_Opt9() {
+		Tournament tour = new Tournament();
+		tour.setType(TournamentType.ELIMINATION, 0, true);
+		tour.generatePlayers(9);
+		tour.schedule();
+		assertEquals(4, tour.getNumberOfRounds());
+	}
+
+	public void testElimination_10() {
+		Tournament tour = new Tournament();
+		tour.setType(TournamentType.ELIMINATION, 3, true);
+		tour.generatePlayers(10);
+		scheduleAll(tour);
 	}
 }
