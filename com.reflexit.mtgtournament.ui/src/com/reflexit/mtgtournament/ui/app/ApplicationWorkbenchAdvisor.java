a8dc71879b12aec9922f436bf6f6890867b27ad3
elaskavaia <elaskavaia>
2009-07-16 01:23:44 +0000

    - updated features list

---
+++ com.reflexit.mtgtournament.ui/src/com/reflexit/mtgtournament/ui/app/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,26 @@
+package com.reflexit.mtgtournament.ui.app;
+
+import org.eclipse.ui.application.IWorkbenchConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+import com.reflexit.mtgtournament.ui.tour.PerspectiveFactoryTournament;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return PerspectiveFactoryTournament.PERSPECTIVE_ID;
+	}
+
+	@Override
+	public void initialize(IWorkbenchConfigurer configurer) {
+		super.initialize(configurer);
+		configurer.setSaveAndRestore(true);
+	}
+}
---
+++ com.reflexit.mtgtournament.ui/src/com/reflexit/mtgtournament/ui/app/TournamentOrganizerApplication.java
@@ -0,0 +1,41 @@
+package com.reflexit.mtgtournament.ui.app;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
+
+public class TournamentOrganizerApplication implements IApplication {
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
+	 */
+	public Object start(IApplicationContext context) {
+		Display display = PlatformUI.createDisplay();
+		try {
+			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
+			if (returnCode == PlatformUI.RETURN_RESTART) {
+				return IApplication.EXIT_RESTART;
+			}
+			return IApplication.EXIT_OK;
+		} finally {
+			display.dispose();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.app.IApplication#stop()
+	 */
+	public void stop() {
+		final IWorkbench workbench = PlatformUI.getWorkbench();
+		if (workbench == null)
+			return;
+		final Display display = workbench.getDisplay();
+		display.syncExec(new Runnable() {
+			public void run() {
+				if (!display.isDisposed())
+					workbench.close();
+			}
+		});
+	}
+}
