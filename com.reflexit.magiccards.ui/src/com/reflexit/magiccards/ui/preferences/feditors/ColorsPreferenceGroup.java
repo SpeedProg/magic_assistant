1f4a9a17b5bdad70315bfa93b01887889ec7f931
elaskavaia <elaskavaia>
2009-10-14 20:07:36 +0000

    [2879122 ] - added extra filter for colors

--- com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/preferences/feditors/ColorsPreferenceGroup.java
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/preferences/feditors/ColorsPreferenceGroup.java
@@ -10,6 +10,7 @@ import org.eclipse.swt.widgets.Group;
 
 import java.util.Iterator;
 
+import com.reflexit.magiccards.core.model.ColorTypes;
 import com.reflexit.magiccards.core.model.Colors;
 
 public class ColorsPreferenceGroup extends MFieldEditorPreferencePage {
@@ -20,11 +21,20 @@ public class ColorsPreferenceGroup extends MFieldEditorPreferencePage {
 		this.group = new Group(getFieldEditorParent(), SWT.NONE);
 		this.group.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		this.group.setText("Color");
-		Composite parent = this.group;
+		this.group.setLayout(new GridLayout(2, false));
+		Composite left = new Composite(group, SWT.NONE);
+		Composite right = new Composite(group, SWT.NONE);
+		left.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		right.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		Colors coreColors = Colors.getInstance();
 		for (Iterator iterator = coreColors.getIds().iterator(); iterator.hasNext();) {
 			String id = (String) iterator.next();
-			addCheckBox(id, coreColors.getNameById(id), parent);
+			addCheckBox(id, coreColors.getNameById(id), left);
+		}
+		ColorTypes colorTypes = ColorTypes.getInstance();
+		for (Iterator iterator = colorTypes.getIds().iterator(); iterator.hasNext();) {
+			String id = (String) iterator.next();
+			addCheckBox(id, colorTypes.getNameById(id), right);
 		}
 	}
 
