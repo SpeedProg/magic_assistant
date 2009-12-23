47898134a71b7effc36cdc14808352e601242172
elaskavaia <elaskavaia>
2009-12-23 03:06:25 +0000

    [2919642] - added deck price in info tab

--- com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/lib/ColorControl.java
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/lib/ColorControl.java
@@ -6,15 +6,12 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Iterator;
 
 import com.reflexit.magiccards.core.model.CardGroup;
-import com.reflexit.magiccards.core.model.Colors;
 import com.reflexit.magiccards.core.model.ICardCountable;
-import com.reflexit.magiccards.core.model.IMagicCard;
-import com.reflexit.magiccards.core.model.MagicCardField;
 import com.reflexit.magiccards.core.model.storage.ICardEventManager;
+import com.reflexit.magiccards.core.model.utils.CardStoreUtils;
 import com.reflexit.magiccards.ui.chart.ChartCanvas;
 import com.reflexit.magiccards.ui.chart.IChartGenerator;
 import com.reflexit.magiccards.ui.chart.SpellColors;
@@ -35,7 +32,7 @@ public class ColorControl extends AbstractDeckPage implements IDeckPage {
 	public void updateFromStore() {
 		if (store == null)
 			return;
-		Collection<CardGroup> groups = buildSpellColorStats();
+		Collection<CardGroup> groups = CardStoreUtils.buildSpellColorStats(store);
 		int values[] = new int[groups.size()];
 		String labels[] = new String[groups.size()];
 		int i = 0;
@@ -47,24 +44,6 @@ public class ColorControl extends AbstractDeckPage implements IDeckPage {
 		IChartGenerator gen = new SpellColors(values, labels);
 		spellColorCanvas.setChartGenerator(gen);
 		spellColorCanvas.redraw();
-	}
-
-	protected Collection<CardGroup> buildSpellColorStats() {
-		HashMap<CardGroup, CardGroup> groupsList = new HashMap();
-		for (Object element : store) {
-			IMagicCard elem = (IMagicCard) element;
-			if (elem.getType().contains("Land"))
-				continue;
-			String name = Colors.getColorName(elem.getCost());
-			CardGroup g = new CardGroup(MagicCardField.COST, name);
-			if (groupsList.containsKey(g)) {
-				groupsList.get(g).addCount(1);
-			} else {
-				g.addCount(1);
-				groupsList.put(g, g);
-			}
-		}
-		return groupsList.keySet();
 	}
 
 	@Override
--- com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/lib/InfoControl.java
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/lib/InfoControl.java
@@ -9,27 +9,63 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
+import java.text.DecimalFormat;
+import java.util.Iterator;
+
+import com.reflexit.magiccards.core.model.ICardCountable;
+import com.reflexit.magiccards.core.model.MagicCardPhisical;
+import com.reflexit.magiccards.core.model.storage.IFilteredCardStore;
 import com.reflexit.magiccards.core.model.storage.IStorage;
 import com.reflexit.magiccards.core.model.storage.IStorageContainer;
 import com.reflexit.magiccards.core.model.storage.IStorageInfo;
+import com.reflexit.magiccards.core.model.utils.CardStoreUtils;
 
 public class InfoControl extends AbstractDeckPage implements IDeckPage {
 	private Text text;
+	private Label total;
+	private Label dbprice;
+	String prefix = "Deck";
+	DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+	private Label colors;
 
 	@Override
 	public Composite createContents(Composite parent) {
 		area = super.createContents(parent);
-		area.setLayout(new GridLayout());
+		area.setLayout(new GridLayout(2, false));
+		createStatsArea();
+		createTextArea();
+		return area;
+	}
+
+	private void createStatsArea() {
+		total = createTextLabel("Total Cards: ");
+		dbprice = createTextLabel(prefix + " cost: ");
+		dbprice.setToolTipText("Cost of a deck using Seller's Price column,"
+		        + " in brackets cost of a deck using User's Price column");
+		colors = createTextLabel("Colors: ");
+	}
+
+	private Label createTextLabel(String string) {
 		Label label = new Label(area, SWT.NONE);
-		label.setText("Description:");
+		label.setText(string);
+		Label text = new Label(area, SWT.NONE);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		text.setLayoutData(gd);
+		return text;
+	}
+
+	private void createTextArea() {
+		Label label = new Label(area, SWT.NONE);
+		label.setText(prefix + " description:");
 		text = new Text(area, SWT.WRAP | SWT.BORDER);
-		text.setLayoutData(new GridData(GridData.FILL_BOTH));
+		GridData gd = new GridData(GridData.FILL_BOTH);
+		gd.horizontalSpan = 2;
+		text.setLayoutData(gd);
 		text.addModifyListener(new ModifyListener() {
 			public void modifyText(ModifyEvent e) {
 				setComment(text.getText());
 			}
 		});
-		return area;
 	}
 
 	protected void setComment(String text2) {
@@ -51,6 +87,15 @@ public class InfoControl extends AbstractDeckPage implements IDeckPage {
 	}
 
 	@Override
+	public void setFilteredStore(IFilteredCardStore store) {
+		super.setFilteredStore(store);
+		IStorageInfo si = getInfo();
+		if (si == null)
+			return;
+		prefix = (getInfo().getType().equals(IStorageInfo.DECK_TYPE)) ? "Deck" : "Collection";
+	}
+
+	@Override
 	public void updateFromStore() {
 		IStorageInfo si = getInfo();
 		if (si != null) {
@@ -58,5 +103,19 @@ public class InfoControl extends AbstractDeckPage implements IDeckPage {
 			if (comment != null)
 				text.setText(comment);
 		}
+		if (store instanceof ICardCountable) {
+			total.setText(((ICardCountable) store).getCount() + "");
+		}
+		float cost = 0;
+		float ucost = 0;
+		for (Iterator iterator = store.iterator(); iterator.hasNext();) {
+			MagicCardPhisical elem = (MagicCardPhisical) iterator.next();
+			cost += elem.getDbPrice() * elem.getCount();
+			ucost += elem.getPrice() * elem.getCount();
+			if (elem.getDbPrice() == 0)
+				cost += elem.getPrice() * elem.getCount();
+		}
+		dbprice.setText("$" + decimalFormat.format(cost) + " ($" + decimalFormat.format(ucost) + ")");
+		colors.setText("" + CardStoreUtils.buildColors(store));
 	}
 }
