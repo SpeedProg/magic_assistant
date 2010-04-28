c781e9fa7844447a43b3b04a52453521deb33f6b
elaskavaia <elaskavaia>
2010-04-28 01:35:56 +0000

    - change prices provider

---
+++ com.reflexit.magiccards.core/src/com/reflexit/magiccards/core/seller/FindMagicCardsPrices.java
@@ -0,0 +1,277 @@
+package com.reflexit.magiccards.core.seller;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.reflexit.magiccards.core.model.Editions;
+import com.reflexit.magiccards.core.model.IMagicCard;
+import com.reflexit.magiccards.core.model.MagicCard;
+import com.reflexit.magiccards.core.model.MagicCardFilter;
+import com.reflexit.magiccards.core.model.MagicCardPhisical;
+import com.reflexit.magiccards.core.model.storage.AbstractFilteredCardStore;
+import com.reflexit.magiccards.core.model.storage.ICardStore;
+import com.reflexit.magiccards.core.model.storage.IFilteredCardStore;
+import com.reflexit.magiccards.core.model.storage.IStorage;
+import com.reflexit.magiccards.core.model.storage.IStorageContainer;
+import com.reflexit.magiccards.core.model.storage.MemoryCardStorage;
+import com.reflexit.magiccards.core.model.storage.MemoryCardStore;
+
+public class FindMagicCardsPrices {
+	String baseURL;
+	String cardURL;
+	HashMap<String, String> setIdMap = new HashMap<String, String>();
+
+	public FindMagicCardsPrices() {
+		// http://www.mtgfanatic.com/store/viewproducts.aspx?CatID=217&AffiliateID=44349&PageSize=500
+		baseURL = "http://findmagiccards.com/Find/BySet/${SetAbbr}/Matches.html";
+		cardURL = "http://findmagiccards.com/Cards/${SetAbbr}/${CardName}.html";
+		// hardcoded setIdMap
+		setIdMap.put("Alara Reborn", "AR");
+		setIdMap.put("Conflux", "CF");
+		setIdMap.put("Champions of Kamigawa", "CK");
+		setIdMap.put("Homelands", "HL");
+		setIdMap.put("Legions", "LE");
+		setIdMap.put("Legends", "LG");
+		setIdMap.put("Lorwyn", "LW");
+		setIdMap.put("Mirage", "MR");
+		setIdMap.put("Mirrodin", "MI");
+		setIdMap.put("Morningtide", "MT");
+		setIdMap.put("Time Spiral", "TSN");
+		setIdMap.put("Portal Second Age", "PO2");
+		setIdMap.put("Portal Three Kingdoms", "P3K");
+		setIdMap.put("Revised Edition", "RV");
+		setIdMap.put("Shadowmoor", "SM");
+		setIdMap.put("Shards of Alara", "SA");
+		setIdMap.put("Torment", "TR");
+		setIdMap.put("Unhinged", "UH");
+		setIdMap.put("Time Spiral \"Timeshifted\"", "TSS");
+	}
+
+	public void updateStore(IFilteredCardStore<IMagicCard> fstore, IProgressMonitor monitor) throws IOException {
+		monitor.beginTask("Loading prices...", fstore.getSize() + 10);
+		HashSet<String> sets = new HashSet();
+		for (IMagicCard magicCard : fstore) {
+			String set = magicCard.getSet();
+			sets.add(set);
+		}
+		monitor.worked(5);
+		IStorage storage = null;
+		if (fstore.getCardStore() instanceof IStorageContainer) {
+			storage = ((IStorageContainer) fstore.getCardStore()).getStorage();
+			storage.setAutoCommit(false);
+		}
+		try {
+			for (String set : sets) {
+				String id = findSetId(set);
+				if (monitor.isCanceled())
+					return;
+				if (id != null) {
+					//System.err.println("found " + set + " " + id);
+					HashMap<String, Float> prices = null;
+					try {
+						prices = parse(id);
+					} catch (IOException e) {
+						//
+					}
+					for (IMagicCard magicCard : fstore) {
+						if (monitor.isCanceled())
+							return;
+						String set2 = magicCard.getSet();
+						if (set2.equals(set)) {
+							float price = -1;
+							if (prices != null && prices.containsKey(magicCard.getName())) {
+								Float price1 = prices.get(magicCard.getName());
+								price = (price1.floatValue());
+							} else {
+								// load individual card
+								try {
+									price = parseSingleCard(id, magicCard);
+								} catch (IOException e) {
+									System.err.println("Cannot load card " + e.getMessage() + " " + set);
+								}
+							}
+							if (price > 0) {
+								if (!setIdMap.containsKey(set))
+									setIdMap.put(set, id);
+								if (magicCard instanceof MagicCardPhisical) {
+									((MagicCardPhisical) magicCard).setDbPrice(price);
+								} else if (magicCard instanceof MagicCard) {
+									((MagicCard) magicCard).setDbPrice(price);
+								}
+								fstore.getCardStore().update(magicCard);
+							}
+							monitor.worked(1);
+						}
+					}
+				}
+			}
+		} finally {
+			if (storage != null) {
+				storage.save();
+				storage.setAutoCommit(true);
+			}
+			monitor.worked(5);
+			monitor.done();
+		}
+	}
+
+	private String findSetId(String set) {
+		if (setIdMap.containsKey(set))
+			return setIdMap.get(set);
+		String abbrByName = Editions.getInstance().getAbbrByName(set);
+		if (abbrByName != null && testSetUrl(abbrByName))
+			return abbrByName;
+		if (abbrByName != null && abbrByName.length() > 2) {
+			String t12 = abbrByName.substring(0, 2);
+			if (testSetUrl(t12))
+				return t12;
+		}
+		String words[] = set.split(" ");
+		String title1 = "";
+		String title2 = "";
+		for (String word : words) {
+			char up = (char) Character.toUpperCase(word.getBytes()[0]);
+			title1 += up;
+			if (word.equals("of"))
+				continue;
+			title2 += up;
+		}
+		if (testSetUrl(title1))
+			return title1;
+		if (testSetUrl(title2))
+			return title2;
+		System.err.println("Cannot find prices for " + set);
+		return null;
+	}
+
+	private boolean testSetUrl(String abbr) {
+		String testurl = "http://findmagiccards.com/Cards/" + abbr;
+		try {
+			URL url = new URL(testurl);
+			InputStream openStream = url.openStream();
+			BufferedReader st = new BufferedReader(new InputStreamReader(openStream));
+			st.readLine();
+			st.readLine();
+			String title = st.readLine();
+			if (title.contains("404"))
+				return false;
+			openStream.close();
+			return true;
+		} catch (MalformedURLException e) {
+			return false;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+
+	public HashMap<String, Float> parse(String setId) throws IOException {
+		HashMap<String, Float> res = new HashMap<String, Float>();
+		URL url = new URL(baseURL.toString().replace("${SetAbbr}", setId));
+		InputStream openStream = url.openStream();
+		BufferedReader st = new BufferedReader(new InputStreamReader(openStream));
+		processFile(st, res);
+		st.close();
+		return res;
+	}
+
+	private float parseSingleCard(String setAbbr, IMagicCard magicCard) throws IOException {
+		String name = magicCard.getName().replaceAll("\\W", "_");
+		URL url = new URL(cardURL.toString().replace("${SetAbbr}", setAbbr).replace("${CardName}", name));
+		InputStream openStream = url.openStream();
+		BufferedReader st = new BufferedReader(new InputStreamReader(openStream));
+		try {
+			return processCard(st);
+		} finally {
+			st.close();
+		}
+	}
+	private static final Pattern rowPattern = Pattern.compile("<TR(.*?)</TR>");
+	private static final Pattern rowPattern2 = Pattern.compile( // 
+	        "<TD><a href='[^']*'>(.*)</a>.nbsp;</TD>" // name
+	                + ".*" //
+	                + "<TD>([0-9.]+).nbsp;</TD>$" // price
+	        );
+
+	/*-
+	 * no NL
+	 <TABLE><TR class=defTitleRow><TH><a href='../../../Find/BySet/TSN/Matches/Name.html'>Name</a></TH><TH><a href='../../../Find/BySet/TSN/Matches/Edition.html'>Edition</a></TH><TH><a href='../../../Find/BySet/TSN/Matches/Colour.html'>Colour</a></TH><TH><a href='../../../Find/BySet/TSN/Matches/Type.html'>Type</a></TH><TH><a href='../../../Find/BySet/TSN/Matches/Cost.html'>Cost</a></TH><TH><a href='../../../Find/BySet/TSN/Matches/Power.html'>Power</a></TH><TH><a href='../../../Find/BySet/TSN/Matches/Rarity.html'>Rarity</a></TH></TR>
+	 <TR class=defRowEven>
+	   <TD><a href='../../../Cards/TSN/Academy_Ruins.html'>Academy Ruins</a>&nbsp;</TD>
+	   <TD>TSN&nbsp;</TD>
+	   <TD><font color='#669999'>Land</font>&nbsp;</TD>
+	   <TD>Legendary Land&nbsp;</TD>
+	   <TD>&nbsp;</TD>
+	   <TD>&nbsp;</TD>
+	   <TD><font color='#999966'>Rare</font>&nbsp;</TD>
+	   <TD>1.94&nbsp;</TD></TR>
+	 */
+	private void processFile(BufferedReader st, HashMap<String, Float> res) throws IOException {
+		String line = "";
+		while ((line = st.readLine()) != null) {
+			Matcher matcher = rowPattern.matcher(line);
+			while (matcher.find()) {
+				String row = matcher.group(1);
+				Matcher matcher2 = rowPattern2.matcher(row);
+				if (matcher2.find()) {
+					String name = matcher2.group(1);
+					String price = matcher2.group(2);
+					try {
+						float f = Float.parseFloat(price);
+						res.put(name, f);
+					} catch (NumberFormatException e) {
+						return;
+					}
+				}
+			}
+		}
+	}
+	/*-
+	 * <TD align=right>Price :</TD><TD>$ 1.23&nbsp;&nbsp;</TD>
+	 */
+	private static final Pattern cardPattern = Pattern.compile("<TD align=right>Price :</TD><TD>\\$ ([0-9.]+)");
+
+	private float processCard(BufferedReader st) throws IOException {
+		String line = "";
+		while ((line = st.readLine()) != null) {
+			Matcher matcher = cardPattern.matcher(line);
+			if (matcher.find()) {
+				String price = matcher.group(1);
+				try {
+					float f = Float.parseFloat(price);
+					return f;
+				} catch (NumberFormatException e) {
+					return -1;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) throws IOException {
+		FindMagicCardsPrices prices = new FindMagicCardsPrices();
+		AbstractFilteredCardStore<IMagicCard> fstore = new AbstractFilteredCardStore<IMagicCard>() {
+			MemoryCardStore<MemoryCardStorage<IMagicCard>> store = new MemoryCardStore<MemoryCardStorage<IMagicCard>>();
+
+			public ICardStore getCardStore() {
+				return store;
+			}
+		};
+		MagicCard card = new MagicCard();
+		card.setSet("Time Spiral");
+		card.setName("Amrou Scout");
+		fstore.getCardStore().add(card);
+		fstore.update(new MagicCardFilter());
+		prices.updateStore(fstore, new NullProgressMonitor());
+	}
+}
