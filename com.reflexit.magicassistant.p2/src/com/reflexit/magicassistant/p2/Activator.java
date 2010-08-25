2ef501c476fc9983f7e6bc1db7143e37bbb8b8eb
elaskavaia <elaskavaia>
2010-08-25 02:28:11 +0000

    buckminster and p2 support

---
+++ com.reflexit.magicassistant.p2/src/com/reflexit/magicassistant/p2/Activator.java
@@ -0,0 +1,59 @@
+package com.reflexit.magicassistant.p2;
+
+import org.eclipse.equinox.p2.ui.Policy;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.reflexit.magicassistant.p2"; //$NON-NLS-1$
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		registerP2Policy(context);
+		//getPreferenceStore().addPropertyChangeListener(getPreferenceListener());
+	}
+
+	private void registerP2Policy(BundleContext context) {
+		MaPolicy policy = new MaPolicy();
+		context.registerService(Policy.class.getName(), policy, null);
+	}
+	public class MaPolicy extends Policy {
+		public MaPolicy() {
+			//setRepositoriesVisible(false);
+			setRestartPolicy(RESTART_POLICY_FORCE);
+			setShowDrilldownRequirements(true);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+}
