2cd5681163f764b48c8bdaea62b87cac0c573458
elaskavaia <elaskavaia>
2010-09-11 03:10:10 +0000

    added command for edit properties for deck

---
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/commands/EditDeckPropertiesHandler.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Alena Laskavaia.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Alena Laskavaia - initial API and implementation
+ *******************************************************************************/
+package com.reflexit.magiccards.ui.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.reflexit.magiccards.core.model.nav.CardCollection;
+import com.reflexit.magiccards.core.model.nav.CardElement;
+import com.reflexit.magiccards.ui.MagicUIActivator;
+import com.reflexit.magiccards.ui.dialogs.EditDeckPropertiesDialog;
+import com.reflexit.magiccards.ui.views.lib.DeckView;
+
+/**
+ * @author Alena
+ *
+ */
+public class EditDeckPropertiesHandler extends AbstractHandler {
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		CardCollection f = getApplicableElement();
+		boolean wasOpen = f.isOpen();
+		EditDeckPropertiesDialog dialog = new EditDeckPropertiesDialog(window.getShell(), f.getStorageInfo());
+		if (dialog.open() == Dialog.OK) {
+			if (wasOpen) {
+				try {
+					// Update TODO: ugly
+					DeckView deckView = (DeckView) window.getActivePage().showView(DeckView.ID, f.getFileName(),
+					        IWorkbenchPage.VIEW_ACTIVATE);
+					deckView.reloadData();
+				} catch (PartInitException e) {
+					MagicUIActivator.log(e);
+				}
+			}
+		}
+		return null;
+	}
+
+	public CardCollection getApplicableElement() {
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		if (window == null)
+			return null;
+		ISelection selection = window.getSelectionService().getSelection();
+		if (selection == null || selection.isEmpty() || !(selection instanceof IStructuredSelection)) {
+			return null;
+		}
+		IStructuredSelection iss = (IStructuredSelection) selection;
+		if (iss.size() > 1) {
+			return null;
+		}
+		CardElement f = (CardElement) iss.getFirstElement();
+		if (!(f instanceof CardCollection)) {
+			return null;
+		}
+		return (CardCollection) f;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.commands.AbstractHandler#setEnabled(java.lang.Object)
+	 */
+	@Override
+	public void setEnabled(Object eo) {
+		if (!isApplicable()) {
+			setBaseEnabled(false);
+			return;
+		}
+		setBaseEnabled(true);
+	}
+
+	private boolean isApplicable() {
+		return getApplicableElement() != null;
+	}
+}
