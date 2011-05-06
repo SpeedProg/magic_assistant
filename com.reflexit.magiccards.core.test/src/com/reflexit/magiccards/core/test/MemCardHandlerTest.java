5463dcf38afb5fce74e3eda1791e821f959d898b
elaskavaia <elaskavaia>
2011-05-06 02:55:27 +0000

    Bug: 3293663  Multi Columns sorting

--- com.reflexit.magiccards.core.test/src/com/reflexit/magiccards/core/test/MemCardHandlerTest.java
+++ com.reflexit.magiccards.core.test/src/com/reflexit/magiccards/core/test/MemCardHandlerTest.java
@@ -10,11 +10,13 @@
  *******************************************************************************/
 package com.reflexit.magiccards.core.test;
 
-import org.junit.Test;
-
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import junit.framework.TestCase;
+
+import org.junit.Test;
 
 import com.reflexit.magiccards.core.model.CardGroup;
 import com.reflexit.magiccards.core.model.CardTypes;
@@ -29,9 +31,15 @@ import com.reflexit.magiccards.core.test.assist.MemCardHandler;
 
 /**
  * @author Alena
- *
+ * 
  */
 public class MemCardHandlerTest extends TestCase {
+	private static final String INSTANT = "Instant";
+	private static final String ARTIFACT = "Artifact";
+	private static final String RED_COST = "{R}";
+	private static final String BLACK_COST = "{B}";
+	private static final String WHITE_COST = "{W}";
+	private static final String ONE_COST = "{1}";
 	MemCardHandler deck;
 	MagicCardFilter filter;
 	private MagicCard card1;
@@ -46,73 +54,89 @@ public class MemCardHandlerTest extends TestCase {
 
 	private void add3cards() {
 		card1 = CardGenerator.generateRandomCard();
-		card1.setCost("{B}");
+		card1.setCost(BLACK_COST);
 		card1.setType("Creature - Elf");
 		card2 = CardGenerator.generateRandomCard();
-		card2.setCost("{R}");
+		card2.setCost(RED_COST);
 		card2.setType("Creature - Goblin");
 		card3 = CardGenerator.generateRandomCard();
-		card3.setCost("{W}");
-		card3.setType("Instant");
+		card3.setCost(WHITE_COST);
+		card3.setType(INSTANT);
 		this.deck.getCardStore().add(card1);
 		this.deck.getCardStore().add(card2);
 		this.deck.getCardStore().add(card3);
 	}
 
+	private MagicCard addCard(String color, String type, String name) {
+		MagicCard card;
+		card = CardGenerator.generateRandomCard();
+		card.setCost(color);
+		card.setType(type);
+		card.setName(name);
+		this.deck.getCardStore().add(card);
+		return card;
+	}
+
 	/**
-	 * Test method for {@link com.reflexit.magiccards.core.model.storage.AbstractFilteredCardStore#update(com.reflexit.magiccards.core.model.MagicCardFilter)}.
+	 * Test method for
+	 * {@link com.reflexit.magiccards.core.model.storage.AbstractFilteredCardStore#update(com.reflexit.magiccards.core.model.MagicCardFilter)}
+	 * .
 	 */
 	@Test
 	public void testGetCardGrpups() {
 		add3cards();
-		this.filter.setSortField(MagicCardField.COST);
+		this.filter.setSortField(MagicCardField.COST, true);
 		this.filter.setGroupField(MagicCardField.COST);
 		this.deck.update(this.filter);
 		Object[] cardGroups = this.deck.getCardGroups();
 		assertEquals(3, cardGroups.length);
-		assertEquals("Black", ((CardGroup) cardGroups[0]).getName());
-		assertEquals("Red", ((CardGroup) cardGroups[1]).getName());
-		assertEquals("White", ((CardGroup) cardGroups[2]).getName());
+		assertEquals("White", ((CardGroup) cardGroups[0]).getName());
+		assertEquals("Black", ((CardGroup) cardGroups[1]).getName());
+		assertEquals("Red", ((CardGroup) cardGroups[2]).getName());
+	}
+
+	protected MagicCard[] getFilteredCards() {
+		this.deck.update(this.filter);
+		Object[] cards = this.deck.getElements();
+		List<Object> collection = Arrays.asList(cards);
+		return collection.toArray(new MagicCard[collection.size()]);
 	}
 
 	@Test
 	public void testFilterByColor() {
 		add3cards();
 		HashMap map = new HashMap();
-		String id = Colors.getInstance().getPrefConstant(Colors.getColorName("{B}"));
+		String id = Colors.getInstance().getPrefConstant(Colors.getColorName(BLACK_COST));
 		map.put(id, "true");
 		this.filter.update(map);
-		this.deck.update(this.filter);
-		Object[] cards = this.deck.getElements();
+		Object[] cards = getFilteredCards();
 		assertEquals(1, cards.length);
-		assertEquals("{B}", ((IMagicCard) cards[0]).getCost());
+		assertEquals(BLACK_COST, ((IMagicCard) cards[0]).getCost());
 	}
 
 	@Test
 	public void testFilterByColor2() {
 		add3cards();
 		HashMap map = new HashMap();
-		map.put(Colors.getInstance().getPrefConstant(Colors.getColorName("{B}")), "true");
-		map.put(Colors.getInstance().getPrefConstant(Colors.getColorName("{W}")), "true");
+		map.put(Colors.getInstance().getPrefConstant(Colors.getColorName(BLACK_COST)), "true");
+		map.put(Colors.getInstance().getPrefConstant(Colors.getColorName(WHITE_COST)), "true");
 		this.filter.update(map);
-		this.deck.update(this.filter);
-		Object[] cards = this.deck.getElements();
+		Object[] cards = getFilteredCards();
 		assertEquals(2, cards.length);
-		assertEquals("{B}", ((IMagicCard) cards[0]).getCost());
-		assertEquals("{W}", ((IMagicCard) cards[1]).getCost());
+		assertEquals(BLACK_COST, ((IMagicCard) cards[0]).getCost());
+		assertEquals(WHITE_COST, ((IMagicCard) cards[1]).getCost());
 	}
 
 	@Test
 	public void testFilterByColorAndType() {
 		add3cards();
 		HashMap map = new HashMap();
-		map.put(Colors.getInstance().getPrefConstant(Colors.getColorName("{R}")), "true");
+		map.put(Colors.getInstance().getPrefConstant(Colors.getColorName(RED_COST)), "true");
 		map.put(CardTypes.getInstance().getPrefConstant("Creature"), "true");
 		this.filter.update(map);
-		this.deck.update(this.filter);
-		Object[] cards = this.deck.getElements();
+		Object[] cards = getFilteredCards();
 		assertEquals(1, cards.length);
-		assertEquals("{R}", ((IMagicCard) cards[0]).getCost());
+		assertEquals(RED_COST, ((IMagicCard) cards[0]).getCost());
 	}
 
 	@Test
@@ -122,9 +146,45 @@ public class MemCardHandlerTest extends TestCase {
 		String typeId = FilterHelper.getPrefConstant(FilterHelper.TYPE_LINE, FilterHelper.TEXT_POSTFIX);
 		map.put(typeId, "Elf");
 		this.filter.update(map);
-		this.deck.update(this.filter);
-		Object[] cards = this.deck.getElements();
+		Object[] cards = getFilteredCards();
 		assertEquals(1, cards.length);
-		assertEquals("{B}", ((IMagicCard) cards[0]).getCost());
+		assertEquals(BLACK_COST, ((IMagicCard) cards[0]).getCost());
+	}
+
+	public void testSortByColor() {
+		add3cards();
+		this.filter.setSortField(MagicCardField.COST, true);
+		Object[] cards = getFilteredCards();
+		assertEquals(3, cards.length);
+		assertEquals(WHITE_COST, ((MagicCard) cards[0]).getCost());
+		assertEquals(BLACK_COST, ((MagicCard) cards[1]).getCost());
+		assertEquals(RED_COST, ((MagicCard) cards[2]).getCost());
+	}
+
+	public void testSortByType() {
+		add3cards();
+		this.filter.setSortField(MagicCardField.TYPE, true);
+		Object[] cards = getFilteredCards();
+		assertEquals(3, cards.length);
+		assertEquals(card1, cards[0]);
+		assertEquals(card2, cards[1]);
+		assertEquals(card3, cards[2]);
+	}
+
+	public void testSortBy2() {
+		MagicCard a0 = addCard(RED_COST, INSTANT, "b");
+		MagicCard a1 = addCard(BLACK_COST, INSTANT, "a");
+		MagicCard a2 = addCard(WHITE_COST, INSTANT, "a");
+		MagicCard a3 = addCard(RED_COST, INSTANT, "a3");
+		MagicCard a4 = addCard(ONE_COST, ARTIFACT, "b");
+		this.filter.setSortField(MagicCardField.NAME, true);
+		this.filter.setSortField(MagicCardField.TYPE, true);
+		MagicCard[] cards = getFilteredCards();
+		assertEquals(5, cards.length);
+		assertEquals(a4, cards[0]);
+		assertEquals(a1, cards[1]);
+		assertEquals(a2, cards[2]);
+		assertEquals(a3, cards[3]);
+		assertEquals(a0, cards[4]);
 	}
 }
