b330ccf8d0f49c3b2754c7e123f1e02bc259f8b9
elaskavaia <elaskavaia>
2011-08-17 01:47:26 +0000

    more stuff in quick filter

--- com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/QuickFilterControl.java
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/QuickFilterControl.java
@@ -1,28 +1,38 @@
 package com.reflexit.magiccards.ui.views;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 
+import com.reflexit.magiccards.core.model.CardTypes;
 import com.reflexit.magiccards.core.model.Colors;
 import com.reflexit.magiccards.core.model.FilterHelper;
 import com.reflexit.magiccards.ui.MagicUIActivator;
 import com.reflexit.magiccards.ui.utils.SymbolConverter;
 
 public class QuickFilterControl extends Composite {
+	private static final String ALL_TYPES = "All Types";
+	private static final String ALL_NAMES = "All Names";
 	private Text searchText;
 	private IPreferenceStore store;
 	private Runnable runnable;
+	private Combo typeCombo;
 
 	public QuickFilterControl(Composite composite, Runnable run) {
 		super(composite, SWT.BORDER);
@@ -30,6 +40,7 @@ public class QuickFilterControl extends Composite {
 		createBar(this);
 		setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		this.runnable = run;
+		setFocus();
 	}
 
 	@Override
@@ -42,14 +53,15 @@ public class QuickFilterControl extends Composite {
 			gd.heightHint = SWT.DEFAULT;
 			gd.minimumHeight = 32;
 			setFocus();
-			this.searchText.setSelection(0, this.searchText.getText().length());
 		}
 		getParent().layout(true);
 	}
 
 	@Override
 	public boolean setFocus() {
-		return searchText.setFocus();
+		boolean x = searchText.setFocus();
+		this.searchText.setSelection(0, this.searchText.getText().length());
+		return x;
 	}
 
 	void createBar(Composite comp) {
@@ -63,10 +75,32 @@ public class QuickFilterControl extends Composite {
 		toolbar.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		// createToolBarLabel(toolbar, "Text:");
 		// search field
+		createSearchField(toolbar);
+		createColorButton(toolbar, "White");
+		createColorButton(toolbar, "Blue");
+		createColorButton(toolbar, "Black");
+		createColorButton(toolbar, "Red");
+		createColorButton(toolbar, "Green");
+		// type
+		createTypeField(toolbar);
+		// hide
+		createHideButton(comp);
+	}
+
+	private void createSearchField(ToolBar toolbar) {
 		this.searchText = new Text(toolbar, SWT.BORDER);
-		this.searchText.setText("name filter...");
+		this.searchText.setText(ALL_NAMES);
 		GridData td = new GridData(GridData.FILL_HORIZONTAL);
 		this.searchText.setLayoutData(td);
+		this.searchText.addFocusListener(new FocusListener() {
+			public void focusLost(FocusEvent e) {
+				// nothing
+			}
+
+			public void focusGained(FocusEvent e) {
+				searchText.setSelection(0, searchText.getText().length());
+			}
+		});
 		this.searchText.addModifyListener(new ModifyListener() {
 			public void modifyText(ModifyEvent e) {
 				filterText(searchText.getText());
@@ -80,14 +114,33 @@ public class QuickFilterControl extends Composite {
 		});
 		ToolItem text = new ToolItem(toolbar, SWT.SEPARATOR);
 		text.setControl(this.searchText);
-		text.setWidth(300);
-		createColorButton(toolbar, "White");
-		createColorButton(toolbar, "Blue");
-		createColorButton(toolbar, "Black");
-		createColorButton(toolbar, "Red");
-		createColorButton(toolbar, "Green");
-		// hide
-		createHideButton(comp);
+		text.setWidth(200);
+	}
+
+	private void createTypeField(ToolBar toolbar) {
+		typeCombo = new Combo(toolbar, SWT.BORDER);
+		typeCombo.add(ALL_TYPES);
+		typeCombo.setText(ALL_TYPES);
+		Collection<String> names = CardTypes.getInstance().getLocalizedNames();
+		for (String type : names) {
+			typeCombo.add(type);
+		}
+		GridData td = new GridData(GridData.FILL_HORIZONTAL);
+		typeCombo.setLayoutData(td);
+		typeCombo.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				filterType(typeCombo.getText());
+			}
+		});
+		typeCombo.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				filterType(typeCombo.getText());
+			}
+		});
+		ToolItem item = new ToolItem(toolbar, SWT.SEPARATOR);
+		item.setControl(this.typeCombo);
+		item.setWidth(200);
 	}
 
 	private void createToolBarLabel(ToolBar toolbar, String string) {
@@ -132,6 +185,13 @@ public class QuickFilterControl extends Composite {
 
 	public void setPreferenceStore(IPreferenceStore store) {
 		this.store = store;
+		if (searchText != null) {
+			init();
+		}
+	}
+
+	private void init() {
+		// TODO Auto-generated method stub
 	}
 
 	public void setUpdateHook(Runnable run) {
@@ -139,11 +199,35 @@ public class QuickFilterControl extends Composite {
 	}
 
 	protected void filterText(String text) {
+		if (ALL_NAMES.equals(text))
+			text = "";
 		String textId = FilterHelper.getPrefConstant(FilterHelper.NAME_LINE, FilterHelper.TEXT_POSTFIX);
 		if (text.trim().length() == 0) {
 			this.store.setValue(textId, "");
 		} else {
 			this.store.setValue(textId, "\"" + text + "\"");
+		}
+		runnable.run();
+	}
+
+	protected void filterType(String text) {
+		if (ALL_TYPES.equals(text))
+			text = "";
+		CardTypes coreTypes = CardTypes.getInstance();
+		String selId = null;
+		String textId = FilterHelper.getPrefConstant(FilterHelper.TYPE_LINE, FilterHelper.TEXT_POSTFIX);
+		this.store.setValue(textId, "");
+		for (Iterator iterator = coreTypes.getIds().iterator(); iterator.hasNext();) {
+			String id = (String) iterator.next();
+			store.setValue(id, "false");
+			if (coreTypes.getLocalizedNameById(id).equals(text)) {
+				selId = id;
+			}
+		}
+		if (selId == null) {
+			this.store.setValue(textId, text.trim());
+		} else {
+			store.setValue(selId, "true");
 		}
 		runnable.run();
 	}
