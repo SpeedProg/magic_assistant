e238453d6505d6f65c962e041eb6bda89b20fc49
elaskavaia <elaskavaia>
2011-09-13 03:10:31 +0000

    updated default database  - include all cards and include coll numbers and other details for them

--- com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/MagicDbView.java
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/MagicDbView.java
@@ -1,5 +1,10 @@
 package com.reflexit.magiccards.ui.views;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
@@ -14,8 +19,14 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
 import com.reflexit.magiccards.core.DataManager;
+import com.reflexit.magiccards.core.model.Editions;
+import com.reflexit.magiccards.core.model.IMagicCard;
 import com.reflexit.magiccards.core.model.Location;
+import com.reflexit.magiccards.core.model.MagicCard;
+import com.reflexit.magiccards.core.model.MagicCardField;
+import com.reflexit.magiccards.core.model.storage.ICardStore;
 import com.reflexit.magiccards.core.model.storage.IFilteredCardStore;
+import com.reflexit.magiccards.core.sync.TextPrinter;
 import com.reflexit.magiccards.ui.MagicUIActivator;
 import com.reflexit.magiccards.ui.preferences.MagicDbViewPreferencePage;
 import com.reflexit.magiccards.ui.views.card.CardDescView;
@@ -25,6 +36,7 @@ public class MagicDbView extends AbstractCardsView {
 	public static final String ID = "com.reflexit.magiccards.ui.views.MagicDbView";
 	MenuManager addToDeck;
 	protected Action showPrintings;
+	protected Action exportDatabase;
 
 	/**
 	 * The constructor.
@@ -105,6 +117,55 @@ public class MagicDbView extends AbstractCardsView {
 				}
 			}
 		};
+		exportDatabase = new Action("Export Database") {
+			@Override
+			public void run() {
+				exportDatabase();
+			}
+		};
+	}
+
+	protected void exportDatabase() {
+		ICardStore<IMagicCard> store = getFilteredStore().getCardStore();
+		MagicCardField[] fields = MagicCardField.values();
+		String curset = null;
+		TextPrinter exporter = null;
+		File dir = new File("C:/tmp/madatabase");
+		dir.mkdirs();
+		Editions editions = Editions.getInstance();
+		PrintStream ps = null;
+		for (IMagicCard magicCard : store) {
+			IMagicCard card = magicCard;
+			if (card instanceof MagicCard) {
+				String set = card.getSet();
+				if (set != curset) {
+					if (ps != null) {
+						ps.close();
+					}
+					try {
+						String abbr = editions.getEditionByName(set).getBaseFileName();
+						FileOutputStream fileOutputStream = new FileOutputStream(new File(dir, abbr + ".txt"));
+						ps = new PrintStream(fileOutputStream);
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					exporter.printHeader((MagicCard) card, ps);
+					curset = set;
+				}
+				MagicCard mc = (MagicCard) card;
+				exporter.print(mc, ps);
+			}
+		}
+		if (ps != null) {
+			ps.close();
+		}
+	}
+
+	@Override
+	protected void fillLocalPullDown(IMenuManager manager) {
+		super.fillLocalPullDown(manager);
+		manager.add(exportDatabase); // this is for internal use only
 	}
 
 	@Override
