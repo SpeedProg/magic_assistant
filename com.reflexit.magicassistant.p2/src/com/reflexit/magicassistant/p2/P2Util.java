faaaecc4ad309ae47c0fb04648688c40f345a920
elaskavaia <elaskavaia>
2012-07-05 01:37:22 +0000

    trying to fix updates on startup

--- com.reflexit.magicassistant.p2/src/com/reflexit/magicassistant/p2/P2Util.java
+++ com.reflexit.magicassistant.p2/src/com/reflexit/magicassistant/p2/P2Util.java
@@ -1,5 +1,6 @@
 package com.reflexit.magicassistant.p2;
 
+import java.net.URI;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
@@ -9,6 +10,8 @@ import org.eclipse.equinox.p2.core.IProvisioningAgent;
 import org.eclipse.equinox.p2.operations.ProvisioningJob;
 import org.eclipse.equinox.p2.operations.ProvisioningSession;
 import org.eclipse.equinox.p2.operations.UpdateOperation;
+import org.eclipse.equinox.p2.repository.artifact.IArtifactRepositoryManager;
+import org.eclipse.equinox.p2.repository.metadata.IMetadataRepositoryManager;
 
 public class P2Util {
 	// XXX Check for updates to this application and return a status.
@@ -22,6 +25,7 @@ public class P2Util {
 		// running profile, using the default profile root marker. To change
 		// which installable units are being updated, use the more detailed
 		// constructors.
+		loadRepository(agent);
 		UpdateOperation operation = new UpdateOperation(session);
 		SubMonitor sub = SubMonitor.convert(monitor, "Checking for application updates...", 200);
 		IStatus status = operation.resolveModal(sub.newChild(100));
@@ -51,5 +55,22 @@ public class P2Util {
 			}
 		}
 		return status;
+	}
+
+	public static void loadRepository(IProvisioningAgent agent) {
+		try {
+			URI repoLocation = new URI("http://mtgbrowser.sourceforge.net/update/1.2/");
+			// Load repository manager
+			IMetadataRepositoryManager metadataManager = (IMetadataRepositoryManager) agent
+					.getService(IMetadataRepositoryManager.SERVICE_NAME);
+			// Load artifact manager
+			IArtifactRepositoryManager artifactManager = (IArtifactRepositoryManager) agent
+					.getService(IArtifactRepositoryManager.SERVICE_NAME);
+			// Load repo
+			metadataManager.loadRepository(repoLocation, null);
+			artifactManager.loadRepository(repoLocation, null);
+		} catch (Exception pe) {
+			Activator.getDefault().getLog().log(new Status(1, Activator.PLUGIN_ID, pe.getMessage()));
+		}
 	}
 }
