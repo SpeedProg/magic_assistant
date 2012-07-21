59734b9dc2b7ba725c3e246ec91361a440e3984f
elaskavaia <elaskavaia>
2012-07-21 23:23:40 +0000

    optimized reader/writer of xml

--- com.reflexit.magiccards.core.xml/src/com/reflexit/magiccards/core/xml/data/MagicXmlStreamReader.java
+++ com.reflexit.magiccards.core.xml/src/com/reflexit/magiccards/core/xml/data/MagicXmlStreamReader.java
@@ -1,9 +1,11 @@
 package com.reflexit.magiccards.core.xml.data;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
@@ -26,20 +28,7 @@ public class MagicXmlStreamReader {
 	private static SAXParserFactory factory = SAXParserFactory.newInstance();
 
 	static enum Tag {
-		cards,
-		list,
-		properties,
-		property,
-		mc,
-		mcp,
-		card,
-		name,
-		key,
-		comment,
-		type,
-		entry,
-		string,
-		fake
+		cards, list, properties, property, mc, mcp, card, name, key, comment, type, entry, string, fake
 	}
 
 	static class MagicHandler extends DefaultHandler {
@@ -74,45 +63,45 @@ public class MagicXmlStreamReader {
 			Tag current = Tag.fake;
 			text.delete(0, text.length());
 			switch (state) {
-				case mc:
-					if (qName.equals(Tag.properties.toString())) {
-						current = Tag.properties;
-					}
-					break;
-				case card:
-					break;
-				case mcp:
-					if (qName.equals(Tag.card.toString())) {
-						current = Tag.card;
-					}
-					break;
-				default:
-					current = Tag.valueOf(qName);
-					break;
+			case mc:
+				if (qName.equals(Tag.properties.toString())) {
+					current = Tag.properties;
+				}
+				break;
+			case card:
+				break;
+			case mcp:
+				if (qName.equals(Tag.card.toString())) {
+					current = Tag.card;
+				}
+				break;
+			default:
+				current = Tag.valueOf(qName);
+				break;
 			}
 			switch (current) {
-				case list:
-					store.list = new ArrayList<IMagicCard>();
-					break;
-				case mc:
-					cardm = new MagicCard();
-					break;
-				case mcp:
-					cardp = new MagicCardPhysical(new MagicCard(), null);
-					break;
-				case entry:
-					if (state == Tag.properties) {
-						key = null;
-						value = null;
-					}
-					break;
-				case property:
-					String name = attributes.getValue("name");
-					String value = attributes.getValue("value");
-					store.properties.setProperty(name, value);
-					break;
-				default:
-					break;
+			case list:
+				store.list = new ArrayList<IMagicCard>();
+				break;
+			case mc:
+				cardm = new MagicCard();
+				break;
+			case mcp:
+				cardp = new MagicCardPhysical(new MagicCard(), null);
+				break;
+			case entry:
+				if (state == Tag.properties) {
+					key = null;
+					value = null;
+				}
+				break;
+			case property:
+				String name = attributes.getValue("name");
+				String value = attributes.getValue("value");
+				store.properties.setProperty(name, value);
+				break;
+			default:
+				break;
 			}
 			states.push(state);
 			state = current;
@@ -121,59 +110,59 @@ public class MagicXmlStreamReader {
 		@Override
 		public void endElement(String uri, String localName, String qName) throws SAXException {
 			switch (state) {
-				case card:
-					break;
-				case mcp:
-					store.list.add(cardp);
-					break;
-				case mc:
-					store.list.add(cardm);
-					break;
-				case name:
-					store.name = text.toString();
-					break;
-				case key:
-					store.key = text.toString();
-					break;
-				case comment:
-					store.comment = text.toString();
-					break;
-				case type:
-					store.type = text.toString();
-					break;
-				case entry:
-					cardm.setProperty(key, value);
-					break;
-				case string:
-					if (key == null)
-						key = text.toString();
-					else
-						value = text.toString();
-					break;
-				case properties:
-					break;
-				case fake: {
-					switch (states.peek()) {
-						case mc: {
-							MagicCardField field = mcFields.get(last);
-							cardm.setObjectByField(field, text.toString());
-							break;
-						}
-						case card: {
-							MagicCardField field = mcFields.get(last);
-							cardp.getBase().setObjectByField(field, text.toString());
-							break;
-						}
-						case mcp: {
-							MagicCardFieldPhysical field = mcpFields.get(last);
-							cardp.setObjectByField(field, text.toString());
-							break;
-						}
-						default:
-							break;
-					}
+			case card:
+				break;
+			case mcp:
+				store.list.add(cardp);
+				break;
+			case mc:
+				store.list.add(cardm);
+				break;
+			case name:
+				store.name = text.toString();
+				break;
+			case key:
+				store.key = text.toString();
+				break;
+			case comment:
+				store.comment = text.toString();
+				break;
+			case type:
+				store.type = text.toString();
+				break;
+			case entry:
+				cardm.setProperty(key, value);
+				break;
+			case string:
+				if (key == null)
+					key = text.toString();
+				else
+					value = text.toString();
+				break;
+			case properties:
+				break;
+			case fake: {
+				switch (states.peek()) {
+				case mc: {
+					MagicCardField field = mcFields.get(last);
+					cardm.setObjectByField(field, text.toString());
 					break;
 				}
+				case card: {
+					MagicCardField field = mcFields.get(last);
+					cardp.getBase().setObjectByField(field, text.toString());
+					break;
+				}
+				case mcp: {
+					MagicCardFieldPhysical field = mcpFields.get(last);
+					cardp.setObjectByField(field, text.toString());
+					break;
+				}
+				default:
+					break;
+				}
+				break;
+			}
 			}
 			text.delete(0, text.length());
 			state = states.pop();
@@ -182,16 +171,16 @@ public class MagicXmlStreamReader {
 		@Override
 		public void characters(char[] ch, int start, int length) {
 			switch (state) {
-				case name:
-				case key:
-				case comment:
-				case type:
-				case string:
-				case fake:
-					text.append(ch, start, length);
-					break;
-				default:
-					break;
+			case name:
+			case key:
+			case comment:
+			case type:
+			case string:
+			case fake:
+				text.append(ch, start, length);
+				break;
+			default:
+				break;
 			}
 		}
 	}
@@ -202,7 +191,13 @@ public class MagicXmlStreamReader {
 			object.file = file;
 			SAXParser parser = factory.newSAXParser();
 			MagicHandler handler = new MagicHandler(object);
-			parser.parse(object.file, handler);
+			long size = object.file.length();
+			if (size > Integer.MAX_VALUE)
+				throw new IllegalArgumentException("File " + file + " is to big " + size);
+			byte buffer[] = new byte[(int) size];
+			int len = FileUtils.readFileAsBytes(file, buffer);
+			InputStream st = new ByteArrayInputStream(buffer, 0, len);
+			parser.parse(st, handler);
 			return object;
 		} catch (IOException e) {
 			throw e;
--- com.reflexit.magiccards.core.xml/src/com/reflexit/magiccards/core/xml/data/MagicXmlStreamWriter.java
+++ com.reflexit.magiccards.core.xml/src/com/reflexit/magiccards/core/xml/data/MagicXmlStreamWriter.java
@@ -3,6 +3,7 @@ package com.reflexit.magiccards.core.xml.data;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,9 +12,7 @@ import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
-
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
+import java.util.Stack;
 
 import com.reflexit.magiccards.core.FileUtils;
 import com.reflexit.magiccards.core.model.ICardField;
@@ -21,11 +20,129 @@ import com.reflexit.magiccards.core.model.MagicCard;
 import com.reflexit.magiccards.core.model.MagicCardField;
 import com.reflexit.magiccards.core.model.MagicCardFieldPhysical;
 import com.reflexit.magiccards.core.model.MagicCardPhysical;
-import com.sun.xml.internal.txw2.output.IndentingXMLStreamWriter;
-import com.sun.xml.internal.ws.api.streaming.XMLStreamWriterFactory;
 
 public class MagicXmlStreamWriter {
-	private XMLStreamWriter writer;
+	class MyOutputStreamWriter {
+		final Charset UTF_8 = Charset.forName("utf-8");
+		OutputStream st;
+		int bufSize = 256 * 1024;
+		StringBuilder builder = new StringBuilder(bufSize);
+
+		public MyOutputStreamWriter(File file) throws FileNotFoundException {
+			st = new FileOutputStream(file);
+		}
+
+		public void write(String string) throws IOException {
+			builder.append(string);
+			if (builder.length() > bufSize)
+				dump();
+		}
+
+		private void dump() throws IOException {
+			st.write(builder.toString().getBytes(UTF_8));
+			builder.delete(0, builder.length());
+		}
+
+		public void write(char c) throws IOException {
+			builder.append(c);
+			if (builder.length() > bufSize)
+				dump();
+		}
+
+		public void close() throws IOException {
+			dump();
+			st.close();
+		}
+	}
+
+	class MyXMLStreamWriter {
+		private MyOutputStreamWriter out;
+		private Stack<String> stack;
+
+		public MyXMLStreamWriter(File file) throws FileNotFoundException {
+			this.out = new MyOutputStreamWriter(file);
+			this.stack = new Stack<String>();
+		}
+
+		public void writeStartElement(String string) throws XMLStreamException {
+			try {
+				int indent = stack.size();
+				for (int i = 0; i < indent; i++) {
+					out.write("  ");
+				}
+				out.write('<');
+				out.write(string);
+				out.write('>');
+				stack.push(string);
+			} catch (IOException e) {
+				throw new XMLStreamException(e);
+			}
+		}
+
+		public void writeEndElement() throws XMLStreamException {
+			try {
+				String string = stack.pop();
+				// int indent = stack.size();
+				// for (int i = 0; i < indent; i++) {
+				// out.write("  ");
+				// }
+				out.write("</");
+				out.write(string);
+				out.write('>');
+				out.write('\n');
+			} catch (IOException e) {
+				throw new XMLStreamException(e);
+			}
+		}
+
+		public void writeCharacters(String string) throws XMLStreamException {
+			try {
+				if (string != null) {
+					if (string.indexOf('&') >= 0) {
+						string = string.replaceAll("&", "\\&amp;");
+					}
+					if (string.indexOf('<') >= 0) {
+						string = string.replaceAll("<", "&lt;");
+					}
+					if (string.indexOf('>') >= 0) {
+						string = string.replaceAll(">", "&gt;");
+					}
+					out.write(string);
+				}
+			} catch (IOException e) {
+				throw new XMLStreamException(e);
+			}
+		}
+
+		public void writeDirect(String string) throws XMLStreamException {
+			try {
+				int indent = stack.size();
+				for (int i = 0; i < indent; i++) {
+					out.write("  ");
+				}
+				out.write(string);
+			} catch (IOException e) {
+				throw new XMLStreamException(e);
+			}
+		}
+
+		public void close() {
+			try {
+				out.close();
+			} catch (IOException e) {
+				// ignore
+				e.printStackTrace();
+			}
+		}
+	}
+
+	class XMLStreamException extends Exception {
+		public XMLStreamException(Throwable e) {
+			super(e);
+		}
+	}
+
+	private MyXMLStreamWriter writer;
 
 	MagicXmlStreamWriter() {
 	}
@@ -33,7 +150,7 @@ public class MagicXmlStreamWriter {
 	public synchronized void write(CardCollectionStoreObject object) throws IOException {
 		OutputStream out = new BufferedOutputStream(new FileOutputStream(object.file), 256 * 1024);
 		try {
-			writer = new IndentingXMLStreamWriter(XMLStreamWriterFactory.create(out, Charset.forName("utf-8").toString()));
+			writer = new MyXMLStreamWriter(object.file);
 			try {
 				writer.writeStartElement("cards");
 				writeSimpleElement("name", object.name);
@@ -45,7 +162,8 @@ public class MagicXmlStreamWriter {
 				// list
 				if (object.list != null) {
 					writer.writeStartElement("list");
-					for (Iterator iterator = object.list.iterator(); iterator.hasNext();) {
+					int i = 0;
+					for (Iterator iterator = object.list.iterator(); iterator.hasNext(); i++) {
 						Object o = iterator.next();
 						if (o instanceof MagicCardPhysical) {
 							writer.writeStartElement("mcp");
@@ -65,6 +183,7 @@ public class MagicXmlStreamWriter {
 				e.printStackTrace();
 			}
 		} finally {
+			writer.close();
 			out.close();
 		}
 	}
@@ -74,9 +193,10 @@ public class MagicXmlStreamWriter {
 			writer.writeStartElement("properties");
 			for (Iterator iterator = properties.keySet().iterator(); iterator.hasNext();) {
 				String key = (String) iterator.next();
-				writer.writeEmptyElement("property");
-				writer.writeAttribute("name", key);
-				writer.writeAttribute("value", properties.getProperty(key));
+				// writer.writeEmptyElement("property");
+				// writer.writeAttribute("name", key);
+				// writer.writeAttribute("value", properties.getProperty(key));
+				writer.writeDirect("<property name=\">" + key + "\" value=\"" + properties.getProperty(key) + "\"");
 			}
 			writer.writeEndElement();
 		}
@@ -167,7 +287,7 @@ public class MagicXmlStreamWriter {
 	public static void main(String[] args) throws IOException {
 		String main = "C:\\Develop\\magic\\runtime-magic.product-init\\magiccards\\Collections\\xxx.xml";
 		String alara = "C:\\Develop\\magic\\runtime-magic.product-init\\magiccards\\MagicDB\\Alara_Reborn.xml";
-		CardCollectionStoreObject o = CardCollectionStoreObject.initFromFile(new File(main));
+		CardCollectionStoreObject o = CardCollectionStoreObject.initFromFile(new File(alara));
 		o.file = new File("c:/tmp/test.xml");
 		new MagicXmlStreamWriter().write(o);
 		String x = FileUtils.readFileAsString(new BufferedReader(new FileReader(o.file)));
