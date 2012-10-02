f13b3ef0b9fb372833638207c4803e83320a554d
elaskavaia <elaskavaia>
2012-10-02 03:50:48 +0000

    support for average

--- com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/columns/PowerColumn.java
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/columns/PowerColumn.java
@@ -1,6 +1,8 @@
 package com.reflexit.magiccards.ui.views.columns;
 
+import com.reflexit.magiccards.core.model.CardGroup;
 import com.reflexit.magiccards.core.model.ICardField;
+import com.reflexit.magiccards.core.model.MagicCard;
 
 public class PowerColumn extends GenColumn {
 	private String fullname;
@@ -18,5 +20,19 @@ public class PowerColumn extends GenColumn {
 	@Override
 	public int getColumnWidth() {
 		return 20;
+	}
+
+	@Override
+	public String getText(Object element) {
+		if (element instanceof CardGroup) {
+			CardGroup node = (CardGroup) element;
+			int cc = node.getCreatureCount();
+			if (cc > 0) {
+				String pow = (String) node.getBase().getObjectByField(dataIndex);
+				Float fpow = MagicCard.convertFloat(pow);
+				return String.valueOf(pow) + " (" + String.format("%.1f", 0.009 + fpow / cc) + ")";
+			}
+		}
+		return super.getText(element);
 	}
 }
