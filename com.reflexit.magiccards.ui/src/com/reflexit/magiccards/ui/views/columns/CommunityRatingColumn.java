1f12134b24dee9b1cda1ce689f83e9bd86450cc4
elaskavaia <elaskavaia>
2012-10-16 01:56:50 +0000

    Test and proper aggregation for groups

--- com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/columns/CommunityRatingColumn.java
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/columns/CommunityRatingColumn.java
@@ -2,6 +2,7 @@ package com.reflexit.magiccards.ui.views.columns;
 
 import java.text.DecimalFormat;
 
+import com.reflexit.magiccards.core.model.CardGroup;
 import com.reflexit.magiccards.core.model.IMagicCard;
 import com.reflexit.magiccards.core.model.MagicCardField;
 
@@ -12,10 +13,18 @@ public class CommunityRatingColumn extends GenColumn {
 	public CommunityRatingColumn() {
 		super(MagicCardField.RATING, "Rating");
 	}
+
 	DecimalFormat decimalFormat = new DecimalFormat("#0.000");
 
 	@Override
 	public String getText(Object element) {
+		if (element instanceof CardGroup) {
+			CardGroup m = (CardGroup) element;
+			float rating = m.getCommunityRating();
+			if (rating == 0)
+				return "";
+			return decimalFormat.format(rating / m.getCount());
+		}
 		if (element instanceof IMagicCard) {
 			IMagicCard m = (IMagicCard) element;
 			float rating = m.getCommunityRating();
