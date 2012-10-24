ba6ac15e7bb1c128218589d168796afbb3966123
elaskavaia <elaskavaia>
2012-10-24 03:39:48 +0000

    rewrote to always faill back to sort by id and name

--- com.reflexit.magiccards.core/src/com/reflexit/magiccards/core/model/SortOrder.java
+++ com.reflexit.magiccards.core/src/com/reflexit/magiccards/core/model/SortOrder.java
@@ -2,40 +2,28 @@ package com.reflexit.magiccards.core.model;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Iterator;
 
 @SuppressWarnings("serial")
-public class SortOrder extends ArrayList<MagicCardComparator> implements Comparator {
+public class SortOrder implements Comparator {
+	private ArrayList<MagicCardComparator> order = new ArrayList<MagicCardComparator>();
+	private static int MAX = 7;
+	private static int REM = 2;
+
+	public SortOrder() {
+		// these are always there
+		order.add(new MagicCardComparator(MagicCardField.NAME, true));
+		order.add(new MagicCardComparator(MagicCardField.ID, true));
+	}
+
 	public int compare(Object o1, Object o2) {
 		if (o1 == o2)
 			return 0; // this is only case it is 0
-		SortOrder sortOrder = this;
-		int dir = sortOrder.isAccending() ? 1 : -1;
-		for (int i = sortOrder.size() - 1; i >= 0; i--) {
-			MagicCardComparator elem = sortOrder.get(i);
+		int dir = isAccending() ? 1 : -1;
+		for (int i = size() - 1; i >= 0; i--) {
+			MagicCardComparator elem = get(i);
 			int d = elem.compare(o1, o2);
 			if (d != 0)
 				return d; // no "dir" since comparator has it already
-		}
-		// everything is equal try id's
-		if (o1 instanceof IMagicCard && o2 instanceof IMagicCard) {
-			IMagicCard c1 = (IMagicCard) o1;
-			IMagicCard c2 = (IMagicCard) o2;
-			if (c1.getCardId() != 0 && c2.getCardId() != 0) {
-				int d = c1.getCardId() - c2.getCardId();
-				if (d != 0)
-					return dir * d;
-			}
-		}
-		// everything is equal try name
-		if (o1 instanceof ICard && o2 instanceof ICard) {
-			String name1 = (String) (((ICard) o1).getObjectByField(MagicCardField.NAME));
-			String name2 = (String) ((ICard) o2).getObjectByField(MagicCardField.NAME);
-			if (name1 != null && name2 != null) {
-				int d = name1.compareTo(name2);
-				if (d != 0)
-					return dir * d;
-			}
 		}
 		return dir * (System.identityHashCode(o1) - System.identityHashCode(o2));
 	}
@@ -49,56 +37,79 @@ public class SortOrder extends ArrayList<MagicCardComparator> implements Compara
 	}
 
 	public void setSortField(ICardField sortField, boolean accending) {
-		SortOrder sortOrder = this;
 		MagicCardComparator elem = new MagicCardComparator(sortField, accending);
-		if (sortOrder.contains(elem)) {
-			sortOrder.remove(elem);
+		for (int i = REM; i < size(); i++) {
+			if (elem.equals(get(i))) {
+				remove(elem);
+				break;
+			}
 		}
-		while (sortOrder.size() > 5) {
-			sortOrder.remove(5);
+		while (size() > MAX) {
+			remove(REM);
 		}
-		sortOrder.push(elem);
+		push(elem);
+	}
+
+	public MagicCardComparator getComparator(ICardField sortField) {
+		int size = size();
+		for (int i = REM; i < size; i++) {
+			MagicCardComparator elem = get(i);
+			if (sortField.equals(elem.getField())) {
+				return elem;
+			}
+		}
+		return null;
 	}
 
 	public boolean hasSortField(ICardField sortField) {
-		if (size() == 0)
-			return false;
-		for (Iterator<MagicCardComparator> iterator = iterator(); iterator.hasNext();) {
-			MagicCardComparator comp = iterator.next();
-			if (sortField.equals(comp.getField())) {
-				return true;
-			}
-		}
-		return false;
+		return getComparator(sortField) != null;
 	}
 
 	public boolean isAccending(ICardField sortField) {
-		if (size() == 0)
-			return true;
-		for (Iterator<MagicCardComparator> iterator = iterator(); iterator.hasNext();) {
-			MagicCardComparator comp = iterator.next();
-			if (sortField.equals(comp.getField())) {
-				return comp.isAccending();
-			}
-		}
+		MagicCardComparator comparator = getComparator(sortField);
+		if (comparator != null)
+			return comparator.isAccending();
 		return false; // default to false
 	}
 
 	public boolean isAccending() {
-		if (size() == 0)
-			return true;
 		MagicCardComparator elem = peek();
 		return elem.isAccending();
 	}
 
 	public boolean isTop(ICardField sortField) {
-		if (size() == 0)
-			return false;
 		MagicCardComparator elem = peek();
 		return elem.getField().equals(sortField);
 	}
 
 	private MagicCardComparator peek() {
 		return get(size() - 1);
+	}
+
+	public int size() {
+		return order.size();
+	}
+
+	public MagicCardComparator get(int index) {
+		return order.get(index);
+	}
+
+	public boolean remove(Object o) {
+		return order.remove(o);
+	}
+
+	public boolean add(MagicCardComparator e) {
+		return order.add(e);
+	}
+
+	public boolean isEmpty() {
+		return size() <= 2;
+	}
+
+	public void clear() {
+		int size = size();
+		for (int i = size - 1; i >= REM; i--) {
+			order.remove(i);
+		}
 	}
 }
