c8f668ef1f762b3fbbd1ddbd7f81593c884e78bd
elaskavaia <elaskavaia>
2013-02-26 03:05:26 +0000

    *** empty log message ***

---
+++ com.reflexit.magicassistant.ui.wt.tests/src/com/reflexit/magicassistant/ui/wt/tests/CreateDeck.java
@@ -0,0 +1,48 @@
+package com.reflexit.magicassistant.ui.wt.tests;
+
+import org.eclipse.swt.widgets.Table;
+import com.reflexit.magiccards.ui.views.MagicDbView;
+import com.reflexit.magiccards.ui.views.lib.DeckView;
+import com.reflexit.magiccards.ui.views.nav.CardsNavigatorView;
+import com.windowtester.runtime.IUIContext;
+import com.windowtester.runtime.condition.HasTextCondition;
+import com.windowtester.runtime.swt.condition.shell.ShellShowingCondition;
+import com.windowtester.runtime.swt.locator.ButtonLocator;
+import com.windowtester.runtime.swt.locator.SWTWidgetLocator;
+import com.windowtester.runtime.swt.locator.TableCellLocator;
+import com.windowtester.runtime.swt.locator.eclipse.PullDownMenuItemLocator;
+import com.windowtester.runtime.swt.locator.eclipse.ViewLocator;
+
+public class CreateDeck extends MagicTestCase {
+	public void testAlara() throws Exception {
+		setAlaraRebornInDb();
+		ViewLocator mdbLocator = new ViewLocator(MagicDbView.ID);
+		// ui.click(new SWTWidgetLocator(Table.class, mdbLocator));
+		TableCellLocator tableItemLocator1 = new TableCellLocator(1, 1).in(mdbLocator);
+		ui.click(tableItemLocator1);
+		ui.assertThat(new HasTextCondition(tableItemLocator1, "Anathemancer"));
+	}
+
+	/**
+	 * Main test method.
+	 */
+	public void testCreateDeck() throws Exception {
+		// setAlaraRebornInDb();
+		IUIContext ui = getUI();
+		ui.click(new PullDownMenuItemLocator("New.../Deck", new ViewLocator(CardsNavigatorView.ID)));
+		ui.wait(new ShellShowingCondition(""));
+		ui.enterText("aaa");
+		ui.click(new ButtonLocator("&Finish"));
+		ViewLocator mdbLocator = new ViewLocator(MagicDbView.ID);
+		ui.click(new SWTWidgetLocator(Table.class, mdbLocator));
+		TableCellLocator tableItemLocator1 = new TableCellLocator(1, 2);
+		ui.click(tableItemLocator1);
+		String name = tableItemLocator1.getText(ui);
+		ui.dragTo(new SWTWidgetLocator(Table.class, new ViewLocator(DeckView.ID)));
+		// ui.click(new SWTWidgetLocator(Table.class, new
+		// ViewLocator(DeckView.ID)));
+		TableCellLocator tableItemLocator = new TableCellLocator(1, 2).in(new ViewLocator(DeckView.ID));
+		// ui.assertThat(tableItemLocator.isSelected());
+		ui.assertThat(new HasTextCondition(tableItemLocator, name));
+	}
+}
---
+++ com.reflexit.magicassistant.ui.wt.tests/src/com/reflexit/magicassistant/ui/wt/tests/GroupByCost.java
@@ -0,0 +1,40 @@
+package com.reflexit.magicassistant.ui.wt.tests;
+
+import static com.windowtester.runtime.swt.locator.SWTLocators.treeItem;
+
+import org.eclipse.swt.widgets.TreeItem;
+
+import com.reflexit.magiccards.ui.views.MagicDbView;
+import com.windowtester.runtime.IUIContext;
+import com.windowtester.runtime.WidgetSearchException;
+import com.windowtester.runtime.locator.IWidgetLocator;
+import com.windowtester.runtime.locator.IWidgetReference;
+import com.windowtester.runtime.swt.locator.TreeItemLocator;
+import com.windowtester.runtime.swt.locator.eclipse.PullDownMenuItemLocator;
+import com.windowtester.runtime.swt.locator.eclipse.ViewLocator;
+
+public class GroupByCost extends MagicTestCase {
+	/**
+	 * Main test method.
+	 */
+	public void testGroupByCost() throws Exception {
+		setAlaraRebornInDb();
+		ui.click(new PullDownMenuItemLocator("Group By/Cost", new ViewLocator(MagicDbView.ID)));
+		expand0();
+	}
+
+	protected void expand0() throws WidgetSearchException {
+		IUIContext ui = getUI();
+		ViewLocator magicDbView = new ViewLocator(MagicDbView.ID);
+		TreeItemLocator treeItemLocator = treeItem("2 (29)").in(magicDbView);
+		IWidgetLocator[] findAll = treeItemLocator.findAll(ui);
+		for (int i = 0; i < findAll.length; i++) {
+			IWidgetLocator iWidgetLocator = findAll[i];
+			System.err.println(iWidgetLocator);
+		}
+		IWidgetReference treeNode = (IWidgetReference) (ui.find(treeItemLocator));
+		TreeItem item = (TreeItem) treeNode.getWidget();
+		if (!isExpanded(item))
+			expand(treeNode);
+	}
+}
