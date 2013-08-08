8628b153b73541cfd858d97f0e320b1f79548b31
elaskavaia <elaskavaia>
2013-08-08 02:41:42 +0000

    * Mana icons are bigger now

--- com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/utils/SymbolConverter.java
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/utils/SymbolConverter.java
@@ -1,6 +1,9 @@
 package com.reflexit.magiccards.ui.utils;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -18,17 +21,22 @@ import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 
+import com.reflexit.magiccards.core.FileUtils;
+import com.reflexit.magiccards.core.sync.UpdateCardsFromWeb;
 import com.reflexit.magiccards.ui.MagicUIActivator;
 
 public class SymbolConverter {
+	private static final int SYMBOL_SIZE = 15;
 	private static String bundleBase;
-	static Map manaMap = new HashMap();
+	static Map<String, String> manaMap = new HashMap<String, String>();
 	static {
-		manaMap.put("{U}", "icons/mana/blue_mana.gif");
-		manaMap.put("{W}", "icons/mana/white_mana.gif");
-		manaMap.put("{B}", "icons/mana/black_mana.gif");
-		manaMap.put("{G}", "icons/mana/green_mana.gif");
-		manaMap.put("{R}", "icons/mana/red_mana.gif");
+		manaMap.put("{T}", "icons/tap.gif");
+		manaMap.put("{Q}", "icons/untap.gif");
+		manaMap.put("{U}", "icons/mana/Symbol_U_mana.gif");
+		manaMap.put("{W}", "icons/mana/Symbol_W_mana.gif");
+		manaMap.put("{B}", "icons/mana/Symbol_B_mana.gif");
+		manaMap.put("{G}", "icons/mana/Symbol_G_mana.gif");
+		manaMap.put("{R}", "icons/mana/Symbol_R_mana.gif");
 		manaMap.put("{S}", "icons/mana/Symbol_snow_mana.gif");
 		manaMap.put("{X}", "icons/mana/Symbol_X_mana.gif");
 		manaMap.put("{Y}", "icons/mana/Symbol_Y_mana.gif");
@@ -62,8 +70,6 @@ public class SymbolConverter {
 		manaMap.put("{2/B}", "icons/mana/Symbol_2B_mana.gif");
 		manaMap.put("{2/G}", "icons/mana/Symbol_2G_mana.gif");
 		manaMap.put("{2/R}", "icons/mana/Symbol_2R_mana.gif");
-		manaMap.put("{T}", "icons/tap.gif");
-		manaMap.put("{Q}", "icons/untap.gif");
 		manaMap.put("{UP}", "icons/mana/Symbol_UP_mana.gif");
 		manaMap.put("{WP}", "icons/mana/Symbol_WP_mana.gif");
 		manaMap.put("{BP}", "icons/mana/Symbol_BP_mana.gif");
@@ -75,7 +81,7 @@ public class SymbolConverter {
 		try {
 			URL url = MagicUIActivator.getDefault().getBundle().getEntry("/");
 			bundleBase = FileLocator.resolve(url).toString();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -98,8 +104,8 @@ public class SymbolConverter {
 		if (cachedImage != null)
 			return cachedImage;
 		Display display = Display.getCurrent();
-		int height = 12;
-		Image image = new Image(display, 12 * 7, height);
+		int height = SYMBOL_SIZE;
+		Image image = new Image(display, SYMBOL_SIZE * 7, height);
 		// painting on green to add transparency later
 		Color backColor = display.getSystemColor(SWT.COLOR_GREEN);
 		GC gc = new GC(image);
@@ -129,11 +135,11 @@ public class SymbolConverter {
 		int y_offset = y;
 		while (text.length() > 0) {
 			boolean cut = false;
-			for (Iterator iterator = manaMap.keySet().iterator(); iterator.hasNext() && text.length() > 0;) {
-				String sym = (String) iterator.next();
+			for (Iterator<String> iterator = manaMap.keySet().iterator(); iterator.hasNext() && text.length() > 0;) {
+				String sym = iterator.next();
 				if (text.startsWith(sym)) {
 					try {
-						String im = (String) manaMap.get(sym);
+						String im = manaMap.get(sym);
 						ImageDescriptor imageDescriptor = MagicUIActivator.getImageDescriptor(im);
 						if (imageDescriptor == null) {
 							MagicUIActivator.log("Cannot find images for " + im + " " + text1);
@@ -166,9 +172,9 @@ public class SymbolConverter {
 		// URL url =
 		// Activator.getDefault().getBundle().getResource("icons/blue_mana.gif");
 		if (bundleBase != null) {
-			for (Iterator iterator = manaMap.keySet().iterator(); iterator.hasNext();) {
-				String sym = (String) iterator.next();
-				text = insertCostImages(text, sym, (String) manaMap.get(sym));
+			for (Iterator<String> iterator = manaMap.keySet().iterator(); iterator.hasNext();) {
+				String sym = iterator.next();
+				text = insertCostImages(text, sym, manaMap.get(sym));
 			}
 		}
 		String style = getHtmlStyle(con);
@@ -182,5 +188,44 @@ public class SymbolConverter {
 		if (text == null)
 			return "";
 		return text.replaceAll("\\Q" + sym, "<img src=\"" + icon + "\" alt=\"" + sym + "\">");
+	}
+
+	public static void main(String[] args) throws MalformedURLException {
+		// get mana symbols
+		// http://gatherer.wizards.com/Handlers/Image.ashx?size=small&name=X&type=symbol
+		// ImageCreator creator = ImageCreator.getInstance();
+		File dir = new File("c:/tmp/symbols");
+		dir.mkdir();
+		for (Iterator<String> iterator = manaMap.keySet().iterator(); iterator.hasNext();) {
+			String sym = iterator.next();
+			sym = sym.substring(1);
+			sym = sym.substring(0, sym.length() - 1);
+			sym = sym.replaceAll("\\W", "");
+			System.err.println(sym);
+			String name = sym;
+			saveManaSymbol(dir, name);
+		}
+		saveManaSymbol(dir, "snow");
+		saveManaSymbol(dir, "12");
+		saveManaSymbol(dir, "14");
+		saveManaSymbol(dir, "15");
+	}
+
+	public static void saveManaSymbol(File dir, String name) throws MalformedURLException {
+		URL url = new URL("http://gatherer.wizards.com/Handlers/Image.ashx?size=small&type=symbol&name=" + name);
+		try {
+			InputStream st = UpdateCardsFromWeb.openUrl(url);
+			File f = new File(dir, "Symbol_" + name + "_mana.gif");
+			FileUtils.saveStream(st, f);
+			st.close();
+			if (f.length() == 0) {
+				System.err.println("Error " + f);
+				f.delete();
+			} else
+				System.err.println("Saved " + f);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
