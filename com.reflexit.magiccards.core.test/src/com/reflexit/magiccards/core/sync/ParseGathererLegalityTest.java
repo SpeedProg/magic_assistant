ae6f8ba9e547c2b200c12fa5a83df577265afae2
elaskavaia <elaskavaia>
2013-08-21 01:35:13 +0000

    legality refactoring

--- com.reflexit.magiccards.core.test/src/com/reflexit/magiccards/core/sync/ParseGathererLegalityTest.java
+++ com.reflexit.magiccards.core.test/src/com/reflexit/magiccards/core/sync/ParseGathererLegalityTest.java
@@ -1,11 +1,11 @@
 package com.reflexit.magiccards.core.sync;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Map;
 
 import junit.framework.TestCase;
 
-import com.reflexit.magiccards.core.monitor.ICoreProgressMonitor;
+import com.reflexit.magiccards.core.model.Legality;
 
 public class ParseGathererLegalityTest extends TestCase {
 	private ParseGathererLegality parser;
@@ -15,18 +15,26 @@ public class ParseGathererLegalityTest extends TestCase {
 		parser = new ParseGathererLegality();
 	}
 
-	// public void testLoadHtml() {
-	// parser.setCardId(153981);
-	// parser.setLanguage("Russian");
-	// String html = "";
-	// html = html.replaceAll("\r?\n", " ");
-	// parser.loadHtml(html, new NullProgressMonitor());
-	// assertEquals(172550, parser.getLangCardId());
-	// }
 	public void testLoad() throws IOException {
-		parser.setCardId(193867);
-		parser.load(ICoreProgressMonitor.NONE);
-		HashMap<String, String> map = parser.getLegalityMap();
-		assertEquals("Banned", map.get("Legacy"));
+		Map<String, Legality> map = parser.getCardLegality(193867);
+		assertEquals(Legality.BANNED, map.get("Legacy"));
+	}
+
+	public void testBalance() throws IOException {
+		Map<String, Legality> map = parser.getCardLegality(202501);
+		assertEquals(Legality.BANNED, map.get("Legacy"));
+		assertEquals(Legality.BANNED, map.get("Commander"));
+		assertEquals(Legality.RESTRICTED, map.get("Vintage"));
+		assertEquals(Legality.LEGAL, map.get("Freeform"));
+		assertEquals(null, map.get("Standard"));
+	}
+
+	public void testAngelicWall() throws IOException {
+		Map<String, Legality> map = parser.getCardLegality(370789);
+		assertEquals(Legality.LEGAL, map.get("Legacy"));
+		assertEquals(Legality.LEGAL, map.get("Commander"));
+		assertEquals(Legality.LEGAL, map.get("Vintage"));
+		assertEquals(Legality.LEGAL, map.get("Freeform"));
+		assertEquals(Legality.LEGAL, map.get("Standard"));
 	}
 }
