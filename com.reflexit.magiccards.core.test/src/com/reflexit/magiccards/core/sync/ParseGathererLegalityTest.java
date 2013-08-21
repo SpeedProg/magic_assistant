ff958b8ba9e5a36a5e6515042be5d13805fb8030
elaskavaia <elaskavaia>
2013-08-21 03:09:25 +0000

    more legality refactoring

--- com.reflexit.magiccards.core.test/src/com/reflexit/magiccards/core/sync/ParseGathererLegalityTest.java
+++ com.reflexit.magiccards.core.test/src/com/reflexit/magiccards/core/sync/ParseGathererLegalityTest.java
@@ -1,11 +1,13 @@
 package com.reflexit.magiccards.core.sync;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import junit.framework.TestCase;
 
 import com.reflexit.magiccards.core.model.Legality;
+import com.reflexit.magiccards.core.model.LegalityMap;
 
 public class ParseGathererLegalityTest extends TestCase {
 	private ParseGathererLegality parser;
@@ -16,12 +18,12 @@ public class ParseGathererLegalityTest extends TestCase {
 	}
 
 	public void testLoad() throws IOException {
-		Map<String, Legality> map = parser.getCardLegality(193867);
+		LegalityMap map = parser.getCardLegality(193867);
 		assertEquals(Legality.BANNED, map.get("Legacy"));
 	}
 
 	public void testBalance() throws IOException {
-		Map<String, Legality> map = parser.getCardLegality(202501);
+		LegalityMap map = parser.getCardLegality(202501);
 		assertEquals(Legality.BANNED, map.get("Legacy"));
 		assertEquals(Legality.BANNED, map.get("Commander"));
 		assertEquals(Legality.RESTRICTED, map.get("Vintage"));
@@ -30,11 +32,27 @@ public class ParseGathererLegalityTest extends TestCase {
 	}
 
 	public void testAngelicWall() throws IOException {
-		Map<String, Legality> map = parser.getCardLegality(370789);
+		LegalityMap map = parser.getCardLegality(370789);
 		assertEquals(Legality.LEGAL, map.get("Legacy"));
 		assertEquals(Legality.LEGAL, map.get("Commander"));
 		assertEquals(Legality.LEGAL, map.get("Vintage"));
 		assertEquals(Legality.LEGAL, map.get("Freeform"));
 		assertEquals(Legality.LEGAL, map.get("Standard"));
+	}
+
+	public void testDeck() {
+		LegalityMap map1 = new LegalityMap();
+		map1.put("Standard", Legality.LEGAL);
+		map1.put("Extended", Legality.LEGAL);
+		LegalityMap map2 = new LegalityMap();
+		map2.put("Extended", Legality.RESTRICTED);
+		map2.put("Tribal Wars", Legality.LEGAL);
+		Collection<LegalityMap> maps = new ArrayList<LegalityMap>();
+		maps.add(map1);
+		maps.add(map2);
+		LegalityMap deck = LegalityMap.calculateDeckLegality(maps);
+		assertEquals(Legality.RESTRICTED, deck.get("Extended"));
+		assertEquals(Legality.NOT_LEGAL, deck.get("Standard"));
+		assertEquals(Legality.NOT_LEGAL, deck.get("Tribal Wars"));
 	}
 }
