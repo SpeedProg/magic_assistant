a5172159c5fa28859acec03c33a4131455f28828
elaskavaia <elaskavaia>
2013-10-07 01:05:09 +0000

    layout, status

--- com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/analyzers/DeckLegalityPage2.java
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/analyzers/DeckLegalityPage2.java
@@ -14,6 +14,8 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.fieldassist.ControlDecoration;
+import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
@@ -21,19 +23,20 @@ import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 
 import com.reflexit.magiccards.core.DataManager;
 import com.reflexit.magiccards.core.legality.Format;
 import com.reflexit.magiccards.core.model.CardGroup;
-import com.reflexit.magiccards.core.model.ICardCountable;
 import com.reflexit.magiccards.core.model.ICardField;
 import com.reflexit.magiccards.core.model.ICardGroup;
 import com.reflexit.magiccards.core.model.IMagicCard;
@@ -48,6 +51,7 @@ import com.reflexit.magiccards.core.model.storage.IFilteredCardStore;
 import com.reflexit.magiccards.core.model.storage.IStorageInfo;
 import com.reflexit.magiccards.core.model.storage.MemoryFilteredCardStore;
 import com.reflexit.magiccards.core.model.utils.CardStoreUtils;
+import com.reflexit.magiccards.core.model.utils.CardStoreUtils.CardStats;
 import com.reflexit.magiccards.core.sync.ParseGathererLegality;
 import com.reflexit.magiccards.ui.MagicUIActivator;
 import com.reflexit.magiccards.ui.utils.CoreMonitorAdapter;
@@ -61,9 +65,9 @@ import com.reflexit.magiccards.ui.views.columns.GroupColumn;
 import com.reflexit.magiccards.ui.views.columns.LegalityColumn;
 
 public class DeckLegalityPage2 extends AbstractDeckListPage {
+	private static final Format DEFAULT_FORMAT = Format.STANDARD;
 	private IFilteredCardStore fstore;
-	private static final String DEFAULT_FORMAT = "Standard";
-	private String format = DEFAULT_FORMAT;
+	private Format format = DEFAULT_FORMAT;
 	private ImageAction load;
 	private LegalityMap deckLegalities = new LegalityMap(true); // map format->legality
 	private Combo comboLegality;
@@ -76,6 +80,9 @@ public class DeckLegalityPage2 extends AbstractDeckListPage {
 	private Label colorsSideboard;
 	private Label maxRepeats;
 	private Label rarity;
+	private CheckControlDecoration totalDeco;
+	private CardStats stats;
+	private CheckControlDecoration maxRepeastDeco;
 
 	class ImageAction extends Action {
 		public ImageAction(String name, String iconKey, int style) {
@@ -92,32 +99,72 @@ public class DeckLegalityPage2 extends AbstractDeckListPage {
 		SashForm sashForm = new SashForm(area, SWT.HORIZONTAL);
 		createInfoPanel(sashForm);
 		createCardsTree(sashForm);
-		sashForm.setWeights(new int[] { 30, 70 });
+		sashForm.setWeights(new int[] { 25, 75 });
 		makeActions();
 		return area;
+	}
+
+	abstract class CheckControlDecoration extends ControlDecoration {
+		public CheckControlDecoration(Control control, int position) {
+			super(control, position);
+			FieldDecorationRegistry registry = FieldDecorationRegistry.getDefault();
+			Image newImage = registry.getFieldDecoration(FieldDecorationRegistry.DEC_ERROR).getImage();
+			setImage(newImage);
+			hide();
+		}
+
+		public void updateVisibility() {
+			String error = validate();
+			if (error == null) {
+				hide();
+			} else {
+				setDescriptionText(error);
+				show();
+			}
+		}
+
+		protected abstract String validate();
 	}
 
 	private void createInfoPanel(Composite parent) {
 		info = new Composite(parent, SWT.BORDER);
 		info.setLayout(new GridLayout(2, false));
 		Button update = new Button(info, SWT.PUSH);
-		update.setText("Load Legality...");
+		update.setText("Load Deck Legality...");
 		update.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				performUpdate();
 			}
 		});
-		update.setLayoutData(GridDataFactory.swtDefaults().span(2, 1).create());
-		createBlueLabel("Format");
+		update.setLayoutData(GridDataFactory.fillDefaults().span(2, 1).create());
+		// createBlueLabel("Format");
 		comboLegality = createLegalityCombo(info);
-		comboLegality.setLayoutData(GridDataFactory.fillDefaults().create());
+		comboLegality.setLayoutData(GridDataFactory.fillDefaults().span(2, 1).create());
 		total = createTextLabel("Cards: ");
 		totalSideboard = createTextLabel("Cards (Sideboard): ");
+		maxRepeats = createTextLabel("Max Repeats: ", "How many time each card repeats, excluding basic land (for legality purposes)");
 		colors = createTextLabel("Colors: ");
 		colorsSideboard = createTextLabel("Colors (Sideboard): ");
-		maxRepeats = createTextLabel("Max Repeats: ", "How many time each card repeats, excluding basic land (for legality purposes)");
 		rarity = createTextLabel("Rarity: ");
+		totalDeco = new CheckControlDecoration(total, SWT.LEAD | SWT.CENTER) {
+			@Override
+			protected String validate() {
+				if (stats == null)
+					return null;
+				String err = format.validateDeckCount(stats.mainCount);
+				return err;
+			}
+		};
+		maxRepeastDeco = new CheckControlDecoration(maxRepeats, SWT.LEAD | SWT.CENTER) {
+			@Override
+			protected String validate() {
+				if (stats == null)
+					return null;
+				String err = format.validateCardCount(stats.maxRepeats);
+				return err;
+			}
+		};
 	}
 
 	protected Combo createLegalityCombo(Composite parent) {
@@ -125,11 +172,15 @@ public class DeckLegalityPage2 extends AbstractDeckListPage {
 		comboLegality.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				setFormat(comboLegality.getText());
+				setFormat(getFormat(comboLegality));
 			}
 		});
 		reloadLegalityCombo(comboLegality);
 		return comboLegality;
+	}
+
+	protected String getFormat(Combo combo) {
+		return (String) combo.getData(combo.getText());
 	}
 
 	private Label createTextLabel(String string) {
@@ -181,25 +232,26 @@ public class DeckLegalityPage2 extends AbstractDeckListPage {
 	public void activate() {
 		super.activate();
 		setFStore();
+		deckLegalities = LegalityMap.calculateDeckLegality(fstore.getCardStore());
 		IStorageInfo storageInfo = getStorageInfo();
 		if (storageInfo != null) {
 			String f = storageInfo.getProperty("format");
-			if (f != null) {
-				format = f;
+			if (f != null && f.trim().length() > 0) {
+				format = Format.valueOf(f);
 			} else {
 				format = DEFAULT_FORMAT;
 			}
 			reloadLegalityCombo(comboLegality);
 		}
-		Format.valueOf(format).checkLegality(fstore.getCardStore(), null);
+		updateInfo();
 		ICardGroup root = fstore.getCardGroupRoot();
 		tree.setInput(root);
 		tree.refresh(true);
-		updateInfo();
+		getListControl().updateViewer();
 	}
 
 	private void updateInfo() {
-		CardStoreUtils.CardStats stats = new CardStoreUtils.CardStats(store);
+		stats = new CardStoreUtils.CardStats(store);
 		totalSideboard.setText(String.valueOf(stats.sideboardCount));
 		total.setText(String.valueOf(stats.mainCount));
 		colors.setImage(SymbolConverter.buildCostImage(stats.mainColors));
@@ -212,6 +264,8 @@ public class DeckLegalityPage2 extends AbstractDeckListPage {
 		if (spellCount > 0) {
 			rarity.setText(spell.getRarity());
 		}
+		totalDeco.updateVisibility();
+		maxRepeastDeco.updateVisibility();
 	}
 
 	@Override
@@ -240,7 +294,7 @@ public class DeckLegalityPage2 extends AbstractDeckListPage {
 	}
 
 	public void setFormat(final String f) {
-		format = f;
+		format = Format.valueOf(f);
 		IStorageInfo storageInfo = getStorageInfo();
 		if (storageInfo != null) {
 			storageInfo.setProperty("format", f);
@@ -271,6 +325,11 @@ public class DeckLegalityPage2 extends AbstractDeckListPage {
 					}
 				};
 			}
+
+			@Override
+			public String getStatusMessage() {
+				return DeckLegalityPage2.this.getStatusMessage();
+			}
 		};
 	}
 
@@ -279,17 +338,21 @@ public class DeckLegalityPage2 extends AbstractDeckListPage {
 		columns.add(new CountColumn() {
 			@Override
 			public Color getBackground(Object element) {
-				if (element instanceof ICardCountable) {
-					int count = ((ICardCountable) element).getCount();
-					LegalityMap legalityMap = ((IMagicCard) element).getLegalityMap();
-					if (legalityMap == null)
-						return null;
-					Legality legality = legalityMap.get(format);
-					boolean legal = Format.valueOf(format).isCountLegal(count, legality);
-					if (!legal)
+				if (element instanceof IMagicCard) {
+					String err = format.validateCardOrGroup((IMagicCard) element);
+					if (err != null)
 						return MagicUIActivator.COLOR_PINKINSH;
 				}
 				return super.getBackground(element);
+			}
+
+			@Override
+			public String getToolTipText(Object element) {
+				if (element instanceof IMagicCard) {
+					String err = format.validateCardOrGroup((IMagicCard) element);
+					return err;
+				}
+				return super.getToolTipText(element);
 			}
 		});
 		columns.add(new CostColumn());
@@ -316,7 +379,21 @@ public class DeckLegalityPage2 extends AbstractDeckListPage {
 				return super.getBackground(element);
 			}
 		});
-		columns.add(new GenColumn(MagicCardFieldPhysical.ERROR, "Error"));
+		columns.add(new GenColumn(MagicCardFieldPhysical.ERROR, "Error") {
+			@Override
+			public int getColumnWidth() {
+				return 250;
+			}
+
+			@Override
+			public String getText(Object element) {
+				if (element instanceof IMagicCard) {
+					String err = format.validateCardOrGroup((IMagicCard) element);
+					return err;
+				}
+				return super.getToolTipText(element);
+			}
+		});
 	}
 
 	protected void performUpdate() {
@@ -325,12 +402,9 @@ public class DeckLegalityPage2 extends AbstractDeckListPage {
 				@Override
 				protected IStatus run(IProgressMonitor monitor) {
 					monitor.beginTask("Calculating Legality", 100);
-					Map<Integer, LegalityMap> cardLegalities = calculateCardLegalities(new SubProgressMonitor(monitor, 90));
-					deckLegalities = LegalityMap.calculateDeckLegality(cardLegalities.values());
+					calculateCardLegalities(new SubProgressMonitor(monitor, 90));
 					getControl().getDisplay().asyncExec(new Runnable() {
 						public void run() {
-							// tree.setInput(deckInput);
-							reloadLegalityCombo(comboLegality);
 							activate();
 						}
 					});
@@ -355,10 +429,25 @@ public class DeckLegalityPage2 extends AbstractDeckListPage {
 	protected void reloadLegalityCombo(Combo comboLegality) {
 		comboLegality.removeAll();
 		for (final Format f : deckLegalities.keySet()) {
-			comboLegality.add(f.name());
+			String label = getFormatLabel(f, deckLegalities.get(f));
+			comboLegality.add(label);
+			comboLegality.setData(label, f.name());
 		}
-		if (format.equals(DEFAULT_FORMAT))
-			comboLegality.add(DEFAULT_FORMAT);
-		comboLegality.setText(format);
+		comboLegality.setText(getFormatLabel(format, deckLegalities.get(format)));
+	}
+
+	private String getFormatLabel(Format f, Legality legality) {
+		if (legality == Legality.UNKNOWN)
+			return f.name();
+		return f.name() + " - " + legality.getLabel();
+	}
+
+	@Override
+	public String getStatusMessage() {
+		String err = format.validateLegality(fstore.getCardStore(), stats);
+		if (err == null)
+			return "Format: " + format.name() + " is legal";
+		else
+			return "Format: " + format.name() + " - " + err;
 	}
 }
