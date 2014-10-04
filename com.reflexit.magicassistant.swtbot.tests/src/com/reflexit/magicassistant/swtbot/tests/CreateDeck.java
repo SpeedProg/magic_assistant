296123af9d44b36fe32d571e05c9c93fb8af8cbc
Alena Laskavaia <elaskavaia@Hedgehog>
2014-10-03 21:51:46 -0400

    test fixes

--- com.reflexit.magicassistant.swtbot.tests/src/com/reflexit/magicassistant/swtbot/tests/CreateDeck.java
+++ com.reflexit.magicassistant.swtbot.tests/src/com/reflexit/magicassistant/swtbot/tests/CreateDeck.java
@@ -10,6 +10,8 @@ import org.eclipse.swtbot.swt.finder.keyboard.KeyboardFactory;
 import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTableItem;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -19,23 +21,36 @@ import com.reflexit.magiccards.ui.views.lib.DeckView;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class CreateDeck extends AbstractSwtBotTest {
-	@Test
-	public void testCreateDeck() throws Exception {
+	private SWTBotView dbView;
+	private SWTBotView deckView;
+
+	@Before
+	public void init() {
+		SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
+		dbView = bot.viewById(MagicDbView.ID);
+	}
+
+	public void createDeck(String deckName) {
 		// create a deck
 		bot.menu("File").menu("New...").menu("Deck").click();
 		bot.shell("").activate();
-		bot.sleep(1000);
-		bot.text().setText("aaa");
+		bot.sleep(500);
+		bot.text().setText(deckName);
 		bot.button("Finish").click();
-		bot.sleep(1000);
+		bot.sleep(500);
+		deckView = bot.viewById(DeckView.ID);
+	}
+
+	@After
+	public void deleteDeck() {
+		// XXX
+	}
+
+	@Test
+	public void testDandD() throws Exception {
+		createDeck("deckDnd");
 		// drag a drop card in the new deck
-		SWTBotView deckView = bot.viewById(DeckView.ID);
-		SWTBotView dbView = bot.viewById(MagicDbView.ID);
-		dbView.setFocus();
-		bot.sleep(1000);
-		SWTBot dbbot = dbView.bot();
-		SWTBotTableItem row = dbbot.table().getTableItem(0);
-		row.select();
+		SWTBotTableItem row = selectFirstRowInDb();
 		String name = row.getText(0);
 		// add card using DND
 		new DndUtil(bot.getDisplay()).dragAndDrop(row, deckView);
@@ -45,29 +60,44 @@ public class CreateDeck extends AbstractSwtBotTest {
 		deckView.bot().table().getTableItem(0).select();
 		bot.sleep(500);
 		assertEquals("Total 1 cards. Selected 1", bot.label().getText());
-		// add card using + shortcut (well = actually)
-		SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
 		KeyboardFactory.getSWTKeyboard().pressShortcut(Keystrokes.DELETE);
 		bot.sleep(500);
 		assertEquals("", bot.label().getText());
-		// insert using + shortcut
+	}
+
+	public SWTBotTableItem selectFirstRowInDb() {
 		dbView.setFocus();
-		row = dbbot.table().getTableItem(0);
+		bot.sleep(1000);
+		SWTBot dbbot = dbView.bot();
+		SWTBotTableItem row = dbbot.table().getTableItem(0);
 		row.select();
+		return row;
+	}
+
+	@Test
+	public void testPlus() throws Exception {
+		createDeck("deckPlus");
+		// drag a drop card in the new deck
+		SWTBotTableItem row = selectFirstRowInDb();
+		String name = row.getText(0);
+		// add card using + shortcut (well = actually)
 		KeyboardFactory.getSWTKeyboard().pressShortcut(Keystrokes.toKeys(0, '='));
 		bot.sleep(500);
 		assertEquals(name, deckView.bot().table().getTableItem(0).getText(0));
-		deckView.bot().table().getTableItem(0).select();
-		KeyboardFactory.getSWTKeyboard().pressShortcut(Keystrokes.DELETE);
-		bot.sleep(500);
+	}
+
+	@Test
+	public void testCutAndPaste() throws Exception {
+		createDeck("deckPaste");
+		// drag a drop card in the new deck
+		SWTBotTableItem row = selectFirstRowInDb();
+		String name = row.getText(0);
 		// add card using cut & paste
-		dbView.setFocus();
-		dbbot.table().getTableItem(0).select();
 		KeyboardFactory.getSWTKeyboard().pressShortcut(Keystrokes.toKeys(SWT.CTRL, 'c'));
 		deckView.setFocus();
-		bot.sleep(500);
+		bot.sleep(1000);
 		KeyboardFactory.getSWTKeyboard().pressShortcut(Keystrokes.toKeys(SWT.CTRL, 'v'));
-		bot.sleep(500);
+		bot.sleep(1000);
 		assertEquals(name, deckView.bot().table().getTableItem(0).getText(0));
 		bot.sleep(500);
 	}
