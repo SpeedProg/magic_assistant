ad5429fcbcb34688629ace32c2354ee059f908d4
Alena Laskavaia <elaskavaia@Hedgehog>
2015-04-07 21:42:37 -0400

    removed unused legality page

--- com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/analyzers/DeckLegalityPage2.java
+++ com.reflexit.magiccards.ui/src/com/reflexit/magiccards/ui/views/analyzers/DeckLegalityPage2.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Alena Laskavaia.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Tym The Enchanter - initial API and implementation
+ *    Alena Laskavaia - ui re-design
+ *******************************************************************************/
 package com.reflexit.magiccards.ui.views.analyzers;
 
 import java.io.IOException;
