e9c13a6555856f8d5c5118f9244c11c999b37a3d
Alena Laskavaia <elaskavaia.cdt@gmail.com>
2015-10-24 23:00:13 -0400

    no special filter stuff for gallery view

--- com.reflexit.magiccards.ui.graphics/src/com/reflexit/magiccards/ui/gallery/GalleryListControl.java
+++ com.reflexit.magiccards.ui.graphics/src/com/reflexit/magiccards/ui/gallery/GalleryListControl.java
@@ -3,8 +3,6 @@ package com.reflexit.magiccards.ui.gallery;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
-import com.reflexit.magiccards.core.model.MagicCardField;
-import com.reflexit.magiccards.core.model.MagicCardFilter;
 import com.reflexit.magiccards.core.model.abs.ICard;
 import com.reflexit.magiccards.core.model.storage.IFilteredCardStore;
 import com.reflexit.magiccards.core.model.storage.LocationFilteredCardStore;
@@ -13,13 +11,8 @@ import com.reflexit.magiccards.ui.views.AbstractMagicCardsListControl;
 import com.reflexit.magiccards.ui.views.IMagicColumnViewer;
 
 public class GalleryListControl extends AbstractMagicCardsListControl {
-
 	public GalleryListControl(AbstractCardsView abstractCardsView) {
 		super(abstractCardsView);
-		IFilteredCardStore<ICard> fistore = getFilteredStore();
-		MagicCardFilter filter = fistore.getFilter();
-		// filter.setNameGroupping(false);
-		filter.setGroupFields(MagicCardField.SET);
 	}
 
 	@Override
@@ -49,5 +42,4 @@ public class GalleryListControl extends AbstractMagicCardsListControl {
 	protected String getPreferencePageId() {
 		return GalleryPreferencePage.getId();
 	}
-
 }
