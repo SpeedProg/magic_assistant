422b354267407bb985942447f19a03d9df49f987
SpeedProg <speedprogde@googlemail.com>
2019-01-16 15:20:50 +0100

    improved currencyconverter by adding a fallback currency provider (the one originally used is actually currently down) also added some code that stops requesting for 10min if there was errors on both providers

--- com.reflexit.magiccards.core/src/com/reflexit/magiccards/core/sync/CurrencyConvertor.java
+++ com.reflexit.magiccards.core/src/com/reflexit/magiccards/core/sync/CurrencyConvertor.java
@@ -1,4 +1,4 @@
-package com.reflexit.magiccards.core.sync;
+ package com.reflexit.magiccards.core.sync;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -12,11 +12,19 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Calendar;
 import java.util.Currency;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
 
 import com.reflexit.magiccards.core.DataManager;
 import com.reflexit.magiccards.core.MagicLogger;
@@ -26,16 +34,20 @@ public class CurrencyConvertor {
 	private static final String FILE = "currency.txt";
 	private static HashMap<String, Double> rates = new HashMap<>();
 	private static HashMap<String, Date> dates = new HashMap<>();
+	private static LocalDateTime lastError = null;
 	private static Currency currency = USD;
 	private static SimpleDateFormat DATE_PARSER = new SimpleDateFormat("MM/dd/yyyy hh:mmaa", Locale.ENGLISH);
+	private static JSONParser parser = new JSONParser();
 
 	public static double loadRate(String from, String to) {
 		return loadRate(from + "_" + to);
 	}
 
 	public static double loadRate(String cu) {
+		cu = convertCu(cu);
 		double res = doLoadRate(cu);
 		if (res == 0) {
+			MagicLogger.info("We got a conversion rate of 0 from 1st api, trying again");
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
@@ -43,6 +55,23 @@ public class CurrencyConvertor {
 			}
 			res = doLoadRate(cu);
 		}
+		
+		if (res == 0) {
+			MagicLogger.log("Conversion rate of 1st api was still 0, trying 2nd conversion api");
+			res = doLoadRateApi2(cu);
+			if (res == 0) {
+				MagicLogger.info("We got a conversion rate of 0 from 1st api, trying again");
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					//
+				}
+				res = doLoadRateApi2(cu);
+				if (res == 0)
+					MagicLogger.log("Conversion rate of 2nd api was still 0. Returning 0.");
+			}
+		}
+
 		return res;
 	}
 
@@ -57,10 +86,11 @@ public class CurrencyConvertor {
 			double rate = Double.parseDouble(parts[1].replace("}", ""));
 			rates.put(cu, rate);
 			Date date = Calendar.getInstance().getTime();
-			MagicLogger.log("Convertion rate " + cu + "=" + rate);
+			MagicLogger.info("Convertion rate " + cu + "=" + rate);
 			dates.put(cu, date);
 			return rate;
 		} catch (Exception e) {
+			MagicLogger.log("Failed to get conversion rate from free.currencyconverterapi.com for " + cu);
 			return 0;
 		}
 	}
@@ -69,6 +99,41 @@ public class CurrencyConvertor {
 		URL url = new URL("https://free.currencyconverterapi.com/api/v5/convert?q=" + cu + "&compact=ultra");
 		return url;
 	}
+
+	/**
+	 * Gets exchange rate from exchangeratesapi.io
+	 * @param cu exchange rate string from_to
+	 * @return the exchange rate
+	 */
+	protected static synchronized double doLoadRateApi2(String cu) {
+		String[] currencies = cu.split("_");
+		try {
+			URL url = getURLApi2(currencies[0], currencies[1]);
+			//{"rates":{"GBP":0.89025,"USD":1.1424},"base":"EUR","date":"2019-01-15"}
+			String res = WebUtils.openUrlText(url);
+
+			try {
+				JSONObject baseObject = (JSONObject) parser.parse(res);
+				JSONObject ratesObject = (JSONObject) baseObject.get("rates");
+				double rate = ((Number)ratesObject.get(currencies[1])).doubleValue();
+				MagicLogger.info("Convertion rate " + cu + "=" + rate);
+				rates.put(cu, rate);
+				dates.put(cu, Calendar.getInstance().getTime());
+				return rate;
+			} catch (org.json.simple.parser.ParseException e) {
+				return 0;
+			}
+		} catch (Exception e) {
+			MagicLogger.log("Failed to get conversion rate from api.exchangeratesapi.io for " + cu);
+			return 0;
+		}
+	}
+
+	public static URL getURLApi2(String from, String to) throws MalformedURLException {
+		URL url = new URL("https://api.exchangeratesapi.io/latest?symbols=" + to + "&base="+from);
+		return url;
+	}
+
 
 	public static synchronized void update() {
 		if (WebUtils.isWorkOffline())
@@ -204,7 +269,14 @@ public class CurrencyConvertor {
 		return currency;
 	}
 
-	public static double getRate(String string) {
+	private static double getRate(String string) {
+		LocalDateTime current = LocalDateTime.now();
+		// if the last error is less then 10min ago just return 0.0 rate
+		if (lastError != null && lastError.plus(10, ChronoUnit.MINUTES).isAfter(current)) {
+			MagicLogger.log("There was an exchange rate API error in the last 10min not checking again.");
+			return 0.0d;
+		}
+		string = convertCu(string);
 		Double rate = rates.get(string);
 		if (rate == null) {
 			if (!WebUtils.isWorkOffline()) {
@@ -227,6 +299,8 @@ public class CurrencyConvertor {
 	}
 
 	public static double getRate(Currency c1, Currency c2) {
+		if (c1.getCurrencyCode().equals(c2.getCurrencyCode()))
+			return 1;
 		return getRate(c1.getCurrencyCode() + c2.getCurrencyCode());
 	}
 }
